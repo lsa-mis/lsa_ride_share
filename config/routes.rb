ff274890ffa99581c3173e92d71139b8e47d58cc
Ananta Saple <anantas@umich.edu>
2023-02-28 09:54:12 -0500

    LRA-496 Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,14 @@ Rails.application.routes.draw do
   resources :sites
   resources :programs
   resources :program_managers
-  devise_for :users
   get 'static_pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "static_pages#home"
+
+  devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks", sessions: "users/sessions"} do
+    delete 'sign_out', :to => 'users/sessions#destroy', :as => :destroy_user_session
+  end
 end
