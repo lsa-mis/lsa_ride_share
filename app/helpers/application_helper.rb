ccbdfb303226f9ef58261fb1dfd9eb88b2370fd4
Margarita Barvinok <brita@umich.edu>
2023-03-02 13:32:50 -0500

    remove term_codes array - terms are in a table

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,28 +1,5 @@
 module ApplicationHelper
 
-  def term_codes
-    [
-      ["Fall 2022", "2410"],
-      ["Winter 2023", "2420"],
-      ["Spring 2023", "2430"],
-      ["Spring/Summer 2023", "2440"],
-      ["Summer 2023", "2450"],
-      ["Fall 2023", "2460"],
-      ["Winter 2024", "2470"],
-      ["Spring 2024", "2480"],
-      ["Spring/Summer 2024", "2490"],
-      ["Summer 2024", "2500"],
-      ["Fall 2024", "2510"],
-      ["Winter 2025", "2520"],
-      ["Spring 2025", "2530"],
-      ["Spring/Summer 2025", "2540"],
-      ["Summer 2025", "2550"],
-      ["Fall 2025", "2560"],
-      ["Winter 2026", "2570"],
-      ["Spring 2026", "2580"]
-    ]
-  end
-
   def default_config_questions
     [
       "Number of students using ride share",
