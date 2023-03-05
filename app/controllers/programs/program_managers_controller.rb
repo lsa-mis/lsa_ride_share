700a224811b0056bf4c4d8dddf183b3d3a891c6b
Margarita Barvinok <brita@umich.edu>
2023-03-05 16:53:43 -0500

    add ldap_lookup

--- app/controllers/programs/program_managers_controller.rb
+++ app/controllers/programs/program_managers_controller.rb
@@ -17,20 +17,28 @@ class Programs::ProgramManagersController < ApplicationController
         return
       else
         @program_manager_program.program_managers << @manager
-        redirect_to @program_manager_program, notice: "The program_manager was added"
+        redirect_to @program_manager_program, notice: "The program manager was added"
         return
       end
     else
       program_manager = ProgramManager.new(program_manager_params)
-     
-      respond_to do |format|
-        if program_manager.save
-          @program_manager_program.program_managers << program_manager
-          format.html { redirect_to @program_manager_program, notice: "The program_manager was created" }
-          format.json { render :show, status: :created, location: @program_manager_program }
-        else
-          format.html { render :new, status: :unprocessable_entity }
-          format.json { render json: @program_manager_program.errors, status: :unprocessable_entity }
+      name = LdapLookup.get_simple_name(uniqname)
+      if name.nil?
+        redirect_to new_program_program_manager_path(@program_manager_program), alert: "#{uniqname} is not a correct uniqname"
+        return
+      else
+        program_manager.first_name = name.split(" ").first
+        program_manager.last_name = name.split(" ").last
+      
+        respond_to do |format|
+          if program_manager.save
+            @program_manager_program.program_managers << program_manager
+            format.html { redirect_to @program_manager_program, notice: "The program manager was added" }
+            format.json { render :show, status: :created, location: @program_manager_program }
+          else
+            format.html { render :new, status: :unprocessable_entity }
+            format.json { render json: @program_manager_program.errors, status: :unprocessable_entity }
+          end
         end
       end
     end
