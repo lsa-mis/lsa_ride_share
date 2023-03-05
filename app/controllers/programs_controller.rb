c879cd3604ac267fa4c8715e648dec97dc0309e7
Margarita Barvinok <brita@umich.edu>
2023-03-05 17:09:12 -0500

    remove wrong param

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -147,6 +147,6 @@ class ProgramsController < ApplicationController
     def program_params
       params.require(:program).permit(:active, :title, :term_start, :term_end, :term_id, :subject, :catalog_number, :class_section, 
                                      :number_of_students, :number_of_students_using_ride_share, :pictures_required_start, :pictures_required_end, 
-                                     :non_uofm_passengers, :instructor_id, :admin_access_id, :updated_by, :managers, :config_questions, :add_managers, instructor_attributes: [:uniqname])
+                                     :non_uofm_passengers, :instructor_id, :admin_access_id, :updated_by, :config_questions, :add_managers, instructor_attributes: [:uniqname])
     end
 end
