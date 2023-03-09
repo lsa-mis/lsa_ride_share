5b08de264b640d705946d5349f9936d64ce67fbc
Margarita Barvinok <brita@umich.edu>
2023-03-08 22:59:57 -0500

    fix config_questions links and parameters

--- app/controllers/programs/config_questions_controller.rb
+++ app/controllers/programs/config_questions_controller.rb
@@ -25,7 +25,7 @@ class Programs::ConfigQuestionsController < ApplicationController
   end
 
   def create
-    @config_question = ConfigQuestion.new(config_question_params)
+    @config_question = ConfigQuestion.new(question: params[:config_question][:question])
     @config_question.program_id = params[:program_id]
 
     respond_to do |format|
@@ -50,7 +50,7 @@ class Programs::ConfigQuestionsController < ApplicationController
     @config_question = ConfigQuestion.find(params[:id])
 
     respond_to do |format|
-      if @config_question.update(config_question_params)
+      if @config_question.update(question: params[:config_question][:question])
         format.turbo_stream { redirect_to program_config_questions_path(@config_question_program),
                               notice: "The config_question was added" 
                             }
