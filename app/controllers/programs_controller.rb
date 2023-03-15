a1ab7b1f81f194a3204a5d68c12589d183e798cf
Margarita Barvinok <brita@umich.edu>
2023-03-15 17:21:12 -0400

    add missing parameter

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -146,6 +146,7 @@ class ProgramsController < ApplicationController
     def program_params
       params.require(:program).permit(:active, :title, :term_start, :term_end, :term_id, :subject, :catalog_number, :class_section, 
                                      :number_of_students, :number_of_students_using_ride_share, :pictures_required_start, :pictures_required_end, 
-                                     :non_uofm_passengers, :instructor_id, :mvr_link, :canvas_link, :canvas_course_id, :admin_access_id, :updated_by, instructor_attributes: [:uniqname])
+                                     :non_uofm_passengers, :instructor_id, :mvr_link, :canvas_link, :canvas_course_id, :admin_access_id, :add_managers, 
+                                     :updated_by, instructor_attributes: [:uniqname])
     end
 end
