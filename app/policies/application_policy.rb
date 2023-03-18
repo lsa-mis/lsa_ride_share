fee74050834fc43e42c98efda1423b73619625b6
Margarita Barvinok <brita@umich.edu>
2023-03-18 00:35:44 -0400

    add policies

---
+++ app/policies/application_policy.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+class ApplicationPolicy
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def index?
+    false
+  end
+
+  def show?
+    false
+  end
+
+  def create?
+    false
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    false
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    false
+  end
+
+  def user_in_access_group?
+    access_groups = ['lsa-rideshare-admins']
+    user.membership && (user.membership & access_groups).any?
+  end
+
+  class Scope
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      raise NotImplementedError, "You must define #resolve in #{self.class}"
+    end
+
+    private
+
+    attr_reader :user, :scope
+  end
+end
