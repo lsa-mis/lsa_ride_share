5600730032553f9729e3264caf63a3d1e1bf3259
Margarita Barvinok <brita@umich.edu>
2023-03-19 23:28:27 -0400

    edit pundit methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,29 +1,24 @@
 class ApplicationController < ActionController::Base
-  before_action :set_membership
   include Pundit::Authorization
 
-  rescue_from Pundit::NotAuthorizedError, with: :user_not_in_group
-
-  def user_not_in_group
-    flash[:alert] = "You are not authorized to perform this action."
-    sign_out(current_user)
-    redirect_to root_path
-  end
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  before_action :set_membership
+  after_action :verify_authorized, unless: :devise_controller?
 
   def user_not_authorized
-    flash[:alert] = "Please sign in to perform this action."
+    flash[:alert] = "You are not authorized to perform this action."
     redirect_to(request.referrer || root_path)
+  end
+
+  def auth_user
+    unless user_signed_in?
+      redirect_to root_path, notice: 'You must sign in first!'
+    end
   end
 
   def set_membership
     if user_signed_in?
       current_user.membership = session[:user_memberships]
-      access_groups = ['lsa-rideshare-admins']
-      unless current_user.membership && (current_user.membership & access_groups).any?
-        flash[:alert] = "You are not authorized to perform this action."
-        sign_out(current_user)
-        redirect_to root_path
-      end
     else
       new_user_session_path
     end
