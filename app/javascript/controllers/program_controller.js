2e6abddffc9a78375143c0716047ccecf3878025
Margarita Barvinok <brita@umich.edu>
2023-03-21 17:00:26 -0400

    edit js program controller

--- app/javascript/controllers/program_controller.js
+++ app/javascript/controllers/program_controller.js
@@ -23,23 +23,17 @@ export default class extends Controller {
 
   submitForm(event) {
     var hide = document.getElementById("program_not_course").checked
-    if (hide) {
-      this.course_errorTarget.classList.remove("fields--display")
-        this.course_errorTarget.classList.add("fields--hide")
-        Turbo.navigator.submitForm(this.formTarget)
-      }
-    else {
-      this.course_errorTarget.classList.remove("fields--display")
-      this.course_errorTarget.classList.add("fields--hide")
-      Turbo.navigator.submitForm(this.formTarget)
+    if (!hide) {
+      console.log("display")
       var subject = this.subjectTarget.value
       var catalog_number = this.catalog_numberTarget.value
       var class_section = this.class_sectionTarget.value
-      if(subject == "" && catalog_number == "" && class_section == "") {
+      if(subject == "" || catalog_number == "" || class_section == "") {
         this.course_errorTarget.classList.add("fields--display")
         this.course_errorTarget.classList.remove("fields--hide")
         event.preventDefault()
       }
+      Turbo.navigator.submitForm(this.formTarget)
     }
   }
 }
