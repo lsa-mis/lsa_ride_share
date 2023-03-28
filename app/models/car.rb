45005a45723e05dba17bddf467d80d4b8631d396
Margarita Barvinok <brita@umich.edu>
2023-03-28 12:22:34 -0400

    add car method to show last vehicle report

--- app/models/car.rb
+++ app/models/car.rb
@@ -25,6 +25,12 @@ class Car < ApplicationRecord
   has_rich_text :note
   has_many_attached :initial_damage
 
+  def last_vehicle_report
+    VehicleReport.where(reservation_id: self.reservations.ids).present? ?
+    VehicleReport.where(reservation_id: self.reservations.ids).order(:updated_at).last :
+    nil
+  end
+
   def vehicle_reports_ids
     VehicleReport.where(reservation_id: self.reservations.ids).present? ? 
       VehicleReport.where(reservation_id: self.reservations.ids).pluck(:id).join(",") : 
