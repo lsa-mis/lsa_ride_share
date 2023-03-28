bb85c50859ca1bf8a839e24dc419038bbdfd3ec6
Margarita Barvinok <brita@umich.edu>
2023-03-28 09:29:57 -0400

    remove comments

--- app/policies/vehicle_report_policy.rb
+++ app/policies/vehicle_report_policy.rb
@@ -10,31 +10,6 @@ class VehicleReportPolicy < ApplicationPolicy
 
   def index?
     user_in_access_group?
-    # true
   end
-
-  # def show?
-  #   user_in_access_group?
-  # end
-
-  # def create?
-  #   user_in_access_group?
-  # end
-
-  # def new?
-  #   create?
-  # end
-
-  # def update?
-  #   user_in_access_group?
-  # end
-
-  # def edit?
-  #   update?
-  # end
-
-  # def destroy?
-  #   false
-  # end
 
 end
