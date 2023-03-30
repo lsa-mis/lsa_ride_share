6b6e3de67ca67ce27de5555ac9b3493b83550e05
Margarita Barvinok <brita@umich.edu>
2023-03-30 15:17:55 -0400

    update unit model

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: units
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  ldap_group :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Unit < ApplicationRecord
+  has_many :programs
+  has_many :cars
 end
