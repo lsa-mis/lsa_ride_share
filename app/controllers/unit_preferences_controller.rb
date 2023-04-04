138deb85aa3fc972ba598046ae70accaa8eebb35
Margarita Barvinok <brita@umich.edu>
2023-04-03 23:47:10 -0400

    style the prefs page

--- app/controllers/unit_preferences_controller.rb
+++ app/controllers/unit_preferences_controller.rb
@@ -4,12 +4,12 @@ class UnitPreferencesController < ApplicationController
   # GET /unit_preferences or /unit_preferences.json
   def index
     @unit_preference = UnitPreference.new
-    @unit_preferences = UnitPreference.distinct.pluck(:name, :description)
+    @unit_preferences = UnitPreference.distinct.order(:name).pluck(:name, :description)
     authorize UnitPreference
   end
 
   def unit_prefs
-    @unit_prefs = UnitPreference.where(unit_id: current_user.unit)
+    @unit_prefs = UnitPreference.where(unit_id: current_user.unit).order(:description)
     authorize @unit_prefs
   end
 
@@ -21,9 +21,7 @@ class UnitPreferencesController < ApplicationController
       params[:unit_prefs].each do |unit, p|
         unit_id = unit.to_i
         p.each do |k, v|
-          # if v == "1"
-            UnitPreference.find_by(unit_id: unit_id, name: k).update(value: true)
-          # end
+          UnitPreference.find_by(unit_id: unit_id, name: k).update(value: true)
         end
       end
     end
