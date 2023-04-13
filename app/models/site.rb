fceb5e5169211e2a9e10776d4835a49d92cf6555
Margarita Barvinok <brita@umich.edu>
2023-04-12 22:24:34 -0400

    add validation to sites

--- app/models/site.rb
+++ app/models/site.rb
@@ -17,10 +17,12 @@ class Site < ApplicationRecord
   has_many :programs_sites
   has_many :programs, through: :programs_sites
   has_many :reservations
-  has_many :site_contacts
+  has_many :site_contacts, dependent: :destroy
   has_many :notes, as: :noteable
 
   accepts_nested_attributes_for :site_contacts
+  validates_presence_of :title, :address1, :address2, :city, :state
+  validates :zip_code, presence: true, format: { with: /\d{5}(-\d{4})?/, message: "should be in the form 12345 or 12345-1234"}
 
   def address
     "#{self.address1} #{self.address2} #{self.city} #{self.state} #{self.zip_code}"
