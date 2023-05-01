446dbdb705a77c05853f2214d14adb4059fe34ad
Margarita Barvinok <brita@umich.edu>
2023-05-01 14:19:19 -0400

    add future and current+future scopes to Term model

--- app/models/term.rb
+++ app/models/term.rb
@@ -16,5 +16,7 @@ class Term < ApplicationRecord
 
   scope :sorted, -> { where.not(classes_begin_date: nil).order(:classes_begin_date, classes_end_date: :desc) }
   scope :current, -> { sorted.where(':date BETWEEN classes_begin_date AND classes_end_date', date: Date.today) }
+  scope :future, -> { sorted.where('classes_begin_date > :date', date: Date.today)}
+  scope :current_and_future, -> { current + future }
 
 end
