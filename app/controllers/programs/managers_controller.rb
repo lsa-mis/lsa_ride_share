a89b56c91498995434a656741abfec96b5e9b837
Margarita Barvinok <brita@umich.edu>
2023-05-05 18:20:08 -0400

    improve controllers code

--- app/controllers/programs/managers_controller.rb
+++ app/controllers/programs/managers_controller.rb
@@ -1,21 +1,14 @@
 class Programs::ManagersController < ApplicationController
   before_action :set_manager_program
-  before_action :set_manager, only: %i[ remove_manager_from_program ]
-  include StudentApi
+  before_action :set_manager, only: %i[ new edit_program_managers ]
+  include ManagerApi
 
-  # GET /managers/new
   def new
-    @manager = Manager.new
-    authorize @manager
   end
 
   def edit_program_managers
-    @manager = Manager.new
-    @managers = @manager_program.managers
-    authorize @manager
   end
 
-  # POST /managers or /managers.json
   def create
     if params[:manager_id].present?
       @manager = Manager.find(params[:manager_id])
@@ -26,10 +19,10 @@ class Programs::ManagersController < ApplicationController
       end
     else
       uniqname = manager_params[:uniqname]
-      result = get_name(uniqname)
+      result = get_name(uniqname, @manager_program)
       @manager = Manager.new(manager_params)
       authorize @manager
-      if result['valid']
+      if result['valid'] 
         @manager.first_name = result['first_name']
         @manager.last_name = result['last_name']
         if @manager.save
@@ -39,32 +32,28 @@ class Programs::ManagersController < ApplicationController
         end
       else
         flash.now[:alert] = result['note']
-        @managers = @manager_program.managers.order(:last_name)
         return
       end
     end
-    @managers = @manager_program.managers.order(:last_name)
   end
 
   def remove_manager_from_program
+    @manager = Manager.find(params[:id])
+    authorize @manager
     if @manager_program.managers.delete(@manager)
       flash.now[:notice] = "The manager was removed from the program."
     end
     @manager = Manager.new
-    @managers = @manager_program.managers
-    @all_managers = Manager.all - @managers
   end
 
   private
 
     def set_manager_program
       @manager_program = Program.find(params[:program_id])
-      @managers = @manager_program.managers
-      @all_managers = Manager.all - @managers
     end
 
     def set_manager
-      @manager = Manager.find(params[:id])
+      @manager = Manager.new
       authorize @manager
     end
 
