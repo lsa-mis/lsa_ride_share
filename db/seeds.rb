1cf2843cc05f5aeff8e4696c5c3dd65341ffce7e
Margarita Barvinok <brita@umich.edu>
2023-05-09 08:19:45 -0400

    add more to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,3 +31,5 @@ terms = Term.create!([
 
 Unit.create!(name: "Psychology", ldap_group: "psych.transportation")
 Unit.create!(name: "Residential College", ldap_group: "rc-rideshare-admins-test")
+
+UnitPreference.create!(name: "faculty_survey", description: "Use faculty survey to create programs")
