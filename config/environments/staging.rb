03d33e54619b75f37d5fbf650dac2ed849b60a7d
rsmokeUM <rsmoke@umich.edu>
2023-05-17 10:55:19 -0400

    set staging to use letter-opener-web to prevent errant email being sent

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -62,20 +62,14 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "art_survey_production"
 
-  config.action_mailer.perform_caching = false
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
   host = 'https://rideshare-staging.lsa.umich.edu/'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => 'apikey',
-    :password       => ENV['SENDGRID_API_KEY'],
-    :domain         => 'umich.edu',
-    :enable_starttls_auto => true
-  }
+  config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
+
+  #letter_opener settings
+  config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.perform_deliveries = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
