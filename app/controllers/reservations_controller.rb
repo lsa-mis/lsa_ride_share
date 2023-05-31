33bdc059782f0d4ce51a248e2baf9a2da4cc69ad
Margarita Barvinok <brita@umich.edu>
2023-05-30 20:23:15 -0400

    edit methods

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,6 +1,6 @@
 class ReservationsController < ApplicationController
   before_action :auth_user
-  before_action :set_reservation, only: %i[ show edit update destroy add_passengers remove_passenger ]
+  before_action :set_reservation, only: %i[ show edit update destroy add_drivers add_passengers remove_passenger ]
   before_action :set_terms_and_units
   before_action :set_programs
 
@@ -21,7 +21,6 @@ class ReservationsController < ApplicationController
     else
       @day_start = Date.today
     end
-    @programs = Program.where(unit_id: current_user.unit_ids)
     @students = []
     @sites = []
     @cars = Car.all
@@ -33,18 +32,18 @@ class ReservationsController < ApplicationController
 
   # GET /reservations/1/edit
   def edit
+    @sites = @reservation.program.sites
+    @number_of_seats = 1..Car.maximum(:number_of_seats)
+    @day_start = @reservation.start_time.to_date
+    @cars = Car.all
   end
 
   def get_available_cars
     if params[:day_start].present?
       @day_start = params[:day_start].to_date
-    else
-      @day_start = Date.today
     end
     if params[:number].present?
       @cars = Car.where("number_of_seats >= ?", params[:number])
-    else
-      @cars = Car.all
     end
     if params[:time_start].present?
       @time_start = params[:time_start]
@@ -52,51 +51,40 @@ class ReservationsController < ApplicationController
     if params[:time_end].present?
       @time_end = params[:time_end]
     end
-    unless params[:time_start] == params[:time_end]
+    unless params[:time_start] >= params[:time_end]
       @reserv_begin = Time.zone.parse(params[:day_start] + " " + params[:time_start]).to_datetime
       @reserv_end = Time.zone.parse(params[:day_start] + " " + params[:time_end]).to_datetime
       range = @reserv_begin..@reserv_end
       @cars = available_cars(@cars, range)
     end
     authorize Reservation
-    # turbo_stream.update "available_cars", partial: "available_cars"
-    # render json: @cars
   end
 
   # POST /reservations or /reservations.json
   def create
-    @reservation = Reservation.new
+    @reservation = Reservation.new(reservation_params)
     @reservation.start_time = Time.zone.parse(params[:day_start] + " " + params[:time_start]).to_datetime
     @reservation.end_time = Time.zone.parse(params[:day_start] + " " + params[:time_end]).to_datetime
-    @reservation.program_id = params[:program_id]
-    @reservation.site_id = params[:site_id]
-    @reservation.car_id = params[:reservation][:car_id]
     @reservation.number_of_people_on_trip = params[:number_of_people_on_trip]
     @reservation.reserved_by = current_user.id
     authorize @reservation
     if @reservation.save
       @students = @reservation.program.students 
-      redirect_to add_passengers_path(@reservation), notice: "Reservation was successfully created. Please add passengers."
+      redirect_to add_drivers_path(@reservation), notice: "Reservation was successfully created. Please add drivers."
     else
       @sites = []
       @students = []
+      @number_of_seats = 1..Car.maximum(:number_of_seats)
+      @cars = Car.all
+      @day_start = params[:day_start]
       render :new, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /reservations/1 or /reservations/1.json
   def update
-    if params[:reservation][:driver].present?
-      @reservation.update(driver_id: Student.find(params[:reservation][:driver]).id)
-      @students = @reservation.program.students - @reservation.passengers
-      @students.delete(@reservation.driver)
-      redirect_to add_passengers_path(@reservation)
-      return
-    end
-    if params[:reservation][:backup_driver].present?
-      @reservation.update(backup_driver_id: Student.find(params[:reservation][:backup_driver]).id)
-      @students = @reservation.program.students - @reservation.passengers
-      @students.delete(@reservation.backup_driver)
+    if params[:reservation][:driver_id].present?
+      @reservation.update(reservation_params)
       redirect_to add_passengers_path(@reservation)
       return
     end
@@ -124,11 +112,15 @@ class ReservationsController < ApplicationController
     end
   end
 
-  def add_passengers
+  def add_drivers
+    @drivers = @reservation.program.students.eligible_drivers
     unless is_admin?(current_user)
       driver = Student.find_by(program_id: @reservation.program_id, uniqname: current_user.uniqname)
       @reservation.update(driver_id: driver.id)
     end
+  end
+
+  def add_passengers
     @passengers = @reservation.passengers
     @students = @reservation.program.students - @passengers
     @students.delete(@reservation.driver)
@@ -172,6 +164,7 @@ class ReservationsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def reservation_params
-      params.require(:reservation).permit(:status, :start_time, :end_time, :recurring, :driver_phone, :backup_driver_phone, :number_of_people_on_trip)
+      params.require(:reservation).permit(:status, :start_time, :end_time, :recurring, :driver_id, :driver_phone, :backup_driver_id, :backup_driver_phone, 
+      :number_of_people_on_trip, :program_id, :site_id, :car_id, :reserved_by)
     end
 end
