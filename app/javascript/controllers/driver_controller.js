2483d14d1024365d50e30831de50226c76441c3d
Margarita Barvinok <brita@umich.edu>
2023-06-07 15:36:54 -0400

    add js driver controller

---
+++ app/javascript/controllers/driver_controller.js
@@ -0,0 +1,34 @@
+import { Controller } from "@hotwired/stimulus"
+import { get } from "@rails/request.js"
+
+export default class extends Controller {
+  static targets = ['form',
+    'driver', 'driver_phone', 'driver_error',
+    'backup_driver', 'backup_driver_phone', 'backup_driver_error']
+  connect() {
+    console.log("connect - driver")
+  }
+
+  submitForm(event) {
+    var driver = this.driverTarget.value
+    var driver_phone = this.driver_phoneTarget.value
+    var backup_driver = this.backup_driverTarget.value
+    var backup_driver_phone = this.backup_driver_phoneTarget.value
+
+    if(driver == "" || driver_phone == "") {
+      this.driver_errorTarget.classList.add("fields--display")
+      this.driver_errorTarget.classList.remove("fields--hide")
+      event.preventDefault()
+    } else if (backup_driver != "" && backup_driver_phone == "") {
+      this.backup_driver_errorTarget.classList.add("fields--display")
+      this.backup_driver_errorTarget.classList.remove("fields--hide")
+      this.driver_errorTarget.classList.remove("fields--display")
+      this.driver_errorTarget.classList.add("fields--hide")
+      event.preventDefault()
+    }
+    else{
+      Turbo.navigator.submitForm(this.formTarget)
+    }
+  }
+
+}
