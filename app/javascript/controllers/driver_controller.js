894f50b02581d8c4a90b97076e6f0cdbb5691288
Margarita Barvinok <brita@umich.edu>
2023-06-07 16:50:45 -0400

    validate drivers

--- app/javascript/controllers/driver_controller.js
+++ app/javascript/controllers/driver_controller.js
@@ -4,7 +4,7 @@ import { get } from "@rails/request.js"
 export default class extends Controller {
   static targets = ['form',
     'driver', 'driver_phone', 'driver_error',
-    'backup_driver', 'backup_driver_phone', 'backup_driver_error']
+    'backup_driver', 'backup_driver_phone', 'backup_driver_error', 'equal_drivers_error']
   connect() {
     console.log("connect - driver")
   }
@@ -18,12 +18,27 @@ export default class extends Controller {
     if(driver == "" || driver_phone == "") {
       this.driver_errorTarget.classList.add("fields--display")
       this.driver_errorTarget.classList.remove("fields--hide")
+      this.equal_drivers_errorTarget.classList.remove("fields--display")
+      this.equal_drivers_errorTarget.classList.add("fields--hide")
+      this.backup_driver_errorTarget.classList.remove("fields--display")
+      this.backup_driver_errorTarget.classList.add("fields--hide")
+      event.preventDefault()
+    } else if (driver != '' && driver == backup_driver) {
+      console.log("hellllll")
+      this.equal_drivers_errorTarget.classList.add("fields--display")
+      this.equal_drivers_errorTarget.classList.remove("fields--hide")
+      this.backup_driver_errorTarget.classList.remove("fields--display")
+      this.backup_driver_errorTarget.classList.add("fields--hide")
+      this.driver_errorTarget.classList.remove("fields--display")
+      this.driver_errorTarget.classList.add("fields--hide")
       event.preventDefault()
     } else if (backup_driver != "" && backup_driver_phone == "") {
       this.backup_driver_errorTarget.classList.add("fields--display")
       this.backup_driver_errorTarget.classList.remove("fields--hide")
       this.driver_errorTarget.classList.remove("fields--display")
       this.driver_errorTarget.classList.add("fields--hide")
+      this.equal_drivers_errorTarget.classList.remove("fields--display")
+      this.equal_drivers_errorTarget.classList.add("fields--hide")
       event.preventDefault()
     }
     else{
