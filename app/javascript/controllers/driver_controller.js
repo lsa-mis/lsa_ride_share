60f598c664845d9f96776b7aa05b2c741d0462f8
Margarita Barvinok <brita@umich.edu>
2023-06-07 23:09:47 -0400

    edit js driver controller

--- app/javascript/controllers/driver_controller.js
+++ app/javascript/controllers/driver_controller.js
@@ -2,9 +2,9 @@ import { Controller } from "@hotwired/stimulus"
 import { get } from "@rails/request.js"
 
 export default class extends Controller {
-  static targets = ['form',
+  static targets = ['form', 'number_of_people_on_trip', 'number_of_passengers',
     'driver', 'driver_phone', 'driver_error',
-    'backup_driver', 'backup_driver_phone', 'backup_driver_error', 'equal_drivers_error']
+    'backup_driver', 'backup_driver_phone']
   connect() {
     console.log("connect - driver")
   }
@@ -14,31 +14,28 @@ export default class extends Controller {
     var driver_phone = this.driver_phoneTarget.value
     var backup_driver = this.backup_driverTarget.value
     var backup_driver_phone = this.backup_driver_phoneTarget.value
+    var number = this.number_of_people_on_tripTarget.value - this.number_of_passengersTarget.value  - 1
+    var error_place = document.getElementById('backup_driver_error');
 
     if(driver == "" || driver_phone == "") {
       this.driver_errorTarget.classList.add("fields--display")
       this.driver_errorTarget.classList.remove("fields--hide")
-      this.equal_drivers_errorTarget.classList.remove("fields--display")
-      this.equal_drivers_errorTarget.classList.add("fields--hide")
-      this.backup_driver_errorTarget.classList.remove("fields--display")
-      this.backup_driver_errorTarget.classList.add("fields--hide")
+      error_place.innerHTML = ''
+      event.preventDefault()
+    } else if (backup_driver != "" && number == 0) {
+      error_place.innerHTML = 'Please remove one passenger to get room for the backup driver';
+      this.driver_errorTarget.classList.remove("fields--display")
+      this.driver_errorTarget.classList.add("fields--hide")
       event.preventDefault()
     } else if (driver != '' && driver == backup_driver) {
-      console.log("hellllll")
-      this.equal_drivers_errorTarget.classList.add("fields--display")
-      this.equal_drivers_errorTarget.classList.remove("fields--hide")
-      this.backup_driver_errorTarget.classList.remove("fields--display")
-      this.backup_driver_errorTarget.classList.add("fields--hide")
+      error_place.innerHTML = 'Driver and backup driver should be different';
       this.driver_errorTarget.classList.remove("fields--display")
       this.driver_errorTarget.classList.add("fields--hide")
       event.preventDefault()
     } else if (backup_driver != "" && backup_driver_phone == "") {
-      this.backup_driver_errorTarget.classList.add("fields--display")
-      this.backup_driver_errorTarget.classList.remove("fields--hide")
+      error_place.innerHTML = "Please enter backup driver's phone";
       this.driver_errorTarget.classList.remove("fields--display")
       this.driver_errorTarget.classList.add("fields--hide")
-      this.equal_drivers_errorTarget.classList.remove("fields--display")
-      this.equal_drivers_errorTarget.classList.add("fields--hide")
       event.preventDefault()
     }
     else{
