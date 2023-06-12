ed129c05eb99e08ac74ad50ec2ec549f2a002205
Jocelyn Anderson <jocelynm@umich.edu>
2023-06-12 11:42:08 -0400

    Validation for mileage added to controller

--- app/javascript/controllers/vehiclereport_controller.js
+++ app/javascript/controllers/vehiclereport_controller.js
@@ -1,8 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ['form', 'gas_error', 'gas_start',
-    'gas_end']
+  static targets = ['form', 'gas_error', 'gas_start', 'gas_end', 'mileage_start', 'mileage_end']
   connect() {
     console.log("connect - vehicle report")
   }
@@ -10,22 +9,42 @@ export default class extends Controller {
   submitForm(event) {
     var gas_start = this.gas_startTarget.value
     var gas_end = this.gas_endTarget.value
+    var mileage_start = this.mileage_startTarget.value
+    var mileage_end = this.mileage_endTarget.value
 
     gas_start = Number(gas_start)
     gas_end = Number(gas_end)
 
     console.log(gas_start)
     console.log(gas_end)
+    console.log(mileage_start)
+    console.log(mileage_end)
+
+    var submitForm = true
 
     if(gas_start > 100 || gas_start < 0 || gas_end > 100 || gas_end < 0) {
       this.gas_errorTarget.classList.add("fields--display")
       this.gas_errorTarget.classList.remove("fields--hide")
 
+      submitForm = false
+    }
+
+    if(mileage_end < mileage_start) {
+      this.mileage_errorTarget.classList.add("fields--display")
+      this.mileage_errorTarget.classList.remove("fields--hide")
+
+      submitForm = false
+    }
+
+
+    if(submitForm == false) {
       event.preventDefault()
     }
-    else{
+    else {
       Turbo.navigator.submitForm(this.formTarget)
     }
+
+
   }
 
 }
