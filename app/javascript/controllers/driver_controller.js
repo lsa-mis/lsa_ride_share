c40cff74f7f4c16adbc8542ea6cf7472aa17f7ee
Margarita Barvinok <brita@umich.edu>
2023-06-13 11:20:37 -0400

    fix drivers validation

--- app/javascript/controllers/driver_controller.js
+++ app/javascript/controllers/driver_controller.js
@@ -11,33 +11,43 @@ export default class extends Controller {
   submitForm(event) {
     var driver = this.driverTarget.value
     var driver_phone = this.driver_phoneTarget.value
-    var backup_driver = this.backup_driverTarget.value
-    var backup_driver_phone = this.backup_driver_phoneTarget.value
     var number = this.number_of_people_on_tripTarget.value - this.number_of_passengersTarget.value  - 1
-    var error_place = document.getElementById('backup_driver_error');
+    var submitForm = true
 
     if(driver == "" || driver_phone == "") {
       this.driver_errorTarget.classList.add("fields--display")
       this.driver_errorTarget.classList.remove("fields--hide")
-      error_place.innerHTML = ''
-      event.preventDefault()
-    } else if (backup_driver != "" && number == 0) {
-      error_place.innerHTML = 'Please remove one passenger then add a backup driver';
-      this.driver_errorTarget.classList.remove("fields--display")
-      this.driver_errorTarget.classList.add("fields--hide")
-      event.preventDefault()
-    } else if (driver != '' && driver == backup_driver) {
-      error_place.innerHTML = 'Driver and backup driver should be different';
-      this.driver_errorTarget.classList.remove("fields--display")
-      this.driver_errorTarget.classList.add("fields--hide")
-      event.preventDefault()
-    } else if (backup_driver != "" && backup_driver_phone == "") {
-      error_place.innerHTML = "Please enter backup driver's phone";
-      this.driver_errorTarget.classList.remove("fields--display")
-      this.driver_errorTarget.classList.add("fields--hide")
+      // error_place.innerHTML = ''
+      submitForm = false
+    }
+
+    if (this.number_of_people_on_tripTarget.value > 1) { 
+      var backup_driver = this.backup_driverTarget.value
+      var backup_driver_phone = this.backup_driver_phoneTarget.value
+      var error_place = document.getElementById('backup_driver_error');
+
+      if (backup_driver != "" && number == 0) {
+        error_place.innerHTML = 'Please remove one passenger then add a backup driver';
+        this.driver_errorTarget.classList.remove("fields--display")
+        this.driver_errorTarget.classList.add("fields--hide")
+        submitForm = false
+      } else if (driver != '' && driver == backup_driver) {
+        error_place.innerHTML = 'Driver and backup driver should be different';
+        this.driver_errorTarget.classList.remove("fields--display")
+        this.driver_errorTarget.classList.add("fields--hide")
+        submitForm = false
+      } else if (backup_driver != "" && backup_driver_phone == "") {
+        error_place.innerHTML = "Please enter backup driver's phone";
+        this.driver_errorTarget.classList.remove("fields--display")
+        this.driver_errorTarget.classList.add("fields--hide")
+        submitForm = false
+      }
+    }
+
+    if(submitForm == false) {
       event.preventDefault()
     }
-    else{
+    else {
       Turbo.navigator.submitForm(this.formTarget)
     }
   }
