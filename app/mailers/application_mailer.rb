cb8e44f4e73900048060be646fa3017be96e1280
rsmokeUM <rsmoke@umich.edu>
2023-06-15 19:44:28 -0400

    add defaults

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
   prepend_view_path "app/views/mailers"
   default from: "lsa-rideshare-admins@umich.edu"
+  default to: "lsa-rideshare-admins@umich.edu"
+  default subject: "Mail from the LSA Rideshare App"
   layout "mailer"
 end
