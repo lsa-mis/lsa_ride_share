74e04ec2fc3f457e61cea7192722c37be42f24d6
rsmokeUM <rsmoke@umich.edu>
2023-06-19 14:03:49 -0400

    set email recipient based on unit_preference value

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -1,7 +1,12 @@
 class ReservationMailer < ApplicationMailer
+  # before_action :set_reservation
+  # before_action :set_recipient
+
   def car_reservation_created
     @reservation = params[:reservation]
     @reserved_by_name = User.find(@reservation.reserved_by).display_name
-    mail(to: @reservation.program.unit.unit_preferences.find_by(name: "notification_email").value, subject: "New reservation for program: #{@reservation.program.title}" )
+    @recipient = @reservation.program.unit.unit_preferences.find_by(name: "notification_email").value.presence || "lsa-rideshare-admins@umich.edu"
+    mail(to: @recipient, subject: "New reservation for program: #{@reservation.program.display_name}" )
   end
+
 end
