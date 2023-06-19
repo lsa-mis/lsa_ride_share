eb3f0cea73cff10f9207cb4aea4c9b9bc8ef9ddd
Margarita Barvinok <brita@umich.edu>
2023-06-19 09:19:03 -0400

    edit model

--- app/models/vehicle_report.rb
+++ app/models/vehicle_report.rb
@@ -47,16 +47,14 @@ class VehicleReport < ApplicationRecord
   has_many_attached :image_damages do |attachable|
    attachable.variant :thumb, resize_to_limit: [250, 250]
   end
+
   has_rich_text :comment
   has_rich_text :admin_comment
-
   has_many :notes, as: :noteable
-
   before_save :set_student_status
 
   def image_damages=(attachables)
     attachables = Array(attachables).compact_blank
- 
     if attachables.any?
       attachment_changes["image_damages"] =
         ActiveStorage::Attached::Changes::CreateMany.new("image_damages", self, image_damages.blobs + attachables)
@@ -66,9 +64,7 @@ class VehicleReport < ApplicationRecord
   validates_presence_of :reservation_id, :mileage_start, :gas_start
   validate :acceptable_documents
   validate :check_mileage_end
-
   validates :reservation_id, uniqueness: true
-
 
   scope :data, ->(reports_ids) { reports_ids.present? ? where(id: reports_ids.split(",").map(&:to_i)) : all }
   
@@ -89,93 +85,75 @@ class VehicleReport < ApplicationRecord
   end
 
   def acceptable_documents
-  
     acceptable_types = [
       "application/pdf", "text/plain", "image/jpg", 
       "image/jpeg", "image/png", 
       "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
     ]
-
     image_front_start do
       unless doc.byte_size <= 20.megabyte
         errors.add(:documents, "is too big")
       end
-
       unless acceptable_types.include?(image_front_start.content_type)
         errors.add(:documents, "must be an acceptable file type (pdf,txt,jpg,png,doc)")
       end
     end
-
     image_passenger_start do
       unless doc.byte_size <= 20.megabyte
         errors.add(:documents, "is too big")
       end
-
       unless acceptable_types.include?(image_passenger_start.content_type)
         errors.add(:documents, "must be an acceptable file type (pdf,txt,jpg,png,doc)")
       end
     end
-
     image_back_start do
       unless doc.byte_size <= 20.megabyte
         errors.add(:documents, "is too big")
       end
-
       unless acceptable_types.include?(image_back_start.content_type)
         errors.add(:documents, "must be an acceptable file type (pdf,txt,jpg,png,doc)")
       end
     end
-
     image_back_start do
       unless doc.byte_size <= 20.megabyte
         errors.add(:documents, "is too big")
       end
-
       unless acceptable_types.include?(image_back_start.content_type)
         errors.add(:documents, "must be an acceptable file type (pdf,txt,jpg,png,doc)")
       end
     end
-
     image_front_end do
       unless doc.byte_size <= 20.megabyte
         errors.add(:documents, "is too big")
       end
-
       unless acceptable_types.include?(image_front_end.content_type)
         errors.add(:documents, "must be an acceptable file type (pdf,txt,jpg,png,doc)")
       end
     end
-
     image_driver_end do
       unless doc.byte_size <= 20.megabyte
         errors.add(:documents, "is too big")
       end
-
       unless acceptable_types.include?(image_driver_end.content_type)
         errors.add(:documents, "must be an acceptable file type (pdf,txt,jpg,png,doc)")
       end
     end
-
     image_passenger_end do
       unless doc.byte_size <= 20.megabyte
         errors.add(:documents, "is too big")
       end
-
       unless acceptable_types.include?(image_passenger_end.content_type)
         errors.add(:documents, "must be an acceptable file type (pdf,txt,jpg,png,doc)")
       end
     end
-
     image_back_end do
       unless doc.byte_size <= 20.megabyte
         errors.add(:documents, "is too big")
       end
-
       unless acceptable_types.include?(image_back_end.content_type)
         errors.add(:documents, "must be an acceptable file type (pdf,txt,jpg,png,doc)")
       end
     end
-
   end
 
   def check_mileage_end
@@ -192,28 +170,23 @@ class VehicleReport < ApplicationRecord
     end
   end
 
-
   def all_fields?
-     self.attributes.except("id", "created_at", "updated_at", "updated_by", "created_by", "status", "note", "student_status").all? {|k, v| v.present?} ? true : false
+    self.attributes.except("id", "created_at", "updated_at", "updated_by", "created_by", "status", "note", "student_status", "approved").all? {|k, v| v.present?} ? true : false
   end
 
   def all_images?
-
     unless self.reservation.program.pictures_required_start && self.reservation.program.pictures_required_end
       return true
     end
-
     images_validated_start = false
     images_validated_end = false
-
     if self.reservation.program.pictures_required_start
-        if check_all_start_images?
-          images_validated_start = true
-        end
+      if check_all_start_images?
+        images_validated_start = true
+      end
     else
       images_validated_start = true #set to true because validation not required
     end
-
     if self.reservation.program.pictures_required_end
       if check_all_end_images?
         images_validated_end = true
@@ -221,9 +194,7 @@ class VehicleReport < ApplicationRecord
     else
       images_validated_end = true #set to true because validation not required
     end
-
     return images_validated_start && images_validated_end
-      
   end
 
   def check_all_start_images?
