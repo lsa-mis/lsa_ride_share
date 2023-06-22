1b2922c026e7964b9b439891c9463ac2174b4bd9
Jocelyn Anderson <jocelynm@umich.edu>
2023-06-22 15:09:41 -0400

    updates to schema and model comments

--- app/models/vehicle_report.rb
+++ app/models/vehicle_report.rb
@@ -2,20 +2,21 @@
 #
 # Table name: vehicle_reports
 #
-#  id             :bigint           not null, primary key
-#  reservation_id :bigint           not null
-#  mileage_start  :float
-#  mileage_end    :float
-#  gas_start      :int
-#  gas_end        :int
-#  parking_spot   :string
-#  created_by     :integer
-#  updated_by     :integer
-#  status         :string
-#  created_at     :datetime         not null
-#  updated_at     :datetime         not null
-#  student_status :boolean          default false
-#  approved       :boolean          default false
+#  id                   :bigint           not null, primary key
+#  reservation_id       :bigint           not null
+#  mileage_start        :float
+#  mileage_end          :float
+#  gas_start            :int
+#  gas_end              :int
+#  parking_spot         :string
+#  created_by           :integer
+#  updated_by           :integer
+#  status               :string
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#  student_status       :boolean          default false
+#  approved             :boolean          default false
+#  parking_spot_return  :string
 #
 class VehicleReport < ApplicationRecord
   belongs_to :reservation
