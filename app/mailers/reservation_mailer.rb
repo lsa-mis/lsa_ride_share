ba122164ab6d03a487a9123bae2193950a6cfbed
Margarita Barvinok <brita@umich.edu>
2023-06-27 10:22:29 -0400

    rename variables

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -22,47 +22,47 @@ class ReservationMailer < ApplicationMailer
     mail(to: @recipients, subject: "Reservation approved for program: #{@reservation.program.display_name}" )
   end
 
-  def car_reservation_cancel_admin(reservation, passengers, emails)
-    @passengers = passengers
-    @start_time = show_date_time(reservation.start_time)
-    @end_time = show_date_time(reservation.end_time)
-    if reservation.driver.present?
-      @driver_name = show_driver(reservation)
+  def car_reservation_cancel_admin(cancel_reservation, cancel_passengers, cancel_emails)
+    @passengers = cancel_passengers
+    @start_time = show_date_time(cancel_reservation.start_time)
+    @end_time = show_date_time(cancel_reservation.end_time)
+    if cancel_reservation.driver.present?
+      @driver_name = show_driver(cancel_reservation)
     else
       @driver_name = "Not Selected"
     end
-    if reservation.backup_driver.present?
-      @backup_driver_name = show_backup_driver(reservation)
+    if cancel_reservation.backup_driver.present?
+      @backup_driver_name = show_backup_driver(cancel_reservation)
     else
       @backup_driver_name = "Not Selected"
     end
-    @admin_email = reservation.program.unit.unit_preferences.find_by(name: "notification_email").value.presence || "lsa-rideshare-admins@umich.edu"
-    @reservation = reservation
+    @admin_email = cancel_reservation.program.unit.unit_preferences.find_by(name: "notification_email").value.presence || "lsa-rideshare-admins@umich.edu"
+    @reservation = cancel_reservation
     mail(to: @admin_email, subject: "Reservation canceled for program: #{@reservation.program.display_name}" )
   end
 
-  def car_reservation_cancel_student(reservation, passengers, emails)
-    @contact_phone = reservation.program.unit.unit_preferences.find_by(name: "contact_phone").value.presence || ""
-    @unit_email = reservation.program.unit.unit_preferences.find_by(name: "notification_email").value.presence || "lsa-rideshare-admins@umich.edu"
-    @passengers = passengers
-    @start_time = show_date_time(reservation.start_time)
-    @end_time = show_date_time(reservation.end_time)
-    if reservation.driver.present?
-      @driver_name = show_driver(reservation)
+  def car_reservation_cancel_student(cancel_reservation, cancel_passengers, cancel_emails)
+    @contact_phone = cancel_reservation.program.unit.unit_preferences.find_by(name: "contact_phone").value.presence || ""
+    @unit_email = cancel_reservation.program.unit.unit_preferences.find_by(name: "notification_email").value.presence || "lsa-rideshare-admins@umich.edu"
+    @passengers = cancel_passengers
+    @start_time = show_date_time(cancel_reservation.start_time)
+    @end_time = show_date_time(cancel_reservation.end_time)
+    if cancel_reservation.driver.present?
+      @driver_name = show_driver(cancel_reservation)
     else
       @driver_name = "Not Selected"
     end
-    if reservation.backup_driver.present?
-      @backup_driver_name = show_backup_driver(reservation)
+    if cancel_reservation.backup_driver.present?
+      @backup_driver_name = show_backup_driver(cancel_reservation)
     else
       @backup_driver_name = "Not Selected"
     end
     recipients = []
-    recipients << email_address(reservation.driver) if reservation.driver.present?
-    recipients << email_address(reservation.backup_driver) if reservation.backup_driver.present?
-    recipients << emails if emails.present?
+    recipients << email_address(cancel_reservation.driver) if cancel_reservation.driver.present?
+    recipients << email_address(cancel_reservation.backup_driver) if cancel_reservation.backup_driver.present?
+    recipients << cancel_emails if cancel_emails.present?
     @recipients = recipients.join(", ")
-    @reservation = reservation
+    @reservation = cancel_reservation
     mail(to: @recipients, subject: "Reservation canceled for program: #{@reservation.program.display_name}" )
   end
 
