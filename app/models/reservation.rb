62897eac478cebd0e69639d72abbc4c0cdb39560
Margarita Barvinok <brita@umich.edu>
2023-07-08 16:28:07 -0400

    update reservation  model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -20,6 +20,8 @@
 #  updated_at               :datetime         not null
 #  reserved_by              :integer
 #  approved                 :boolean          default false
+#  non_uofm_passengers      :string
+#  number_of_non_uofm_passengers :integer
 #
 class Reservation < ApplicationRecord
   belongs_to :program
@@ -31,6 +33,7 @@ class Reservation < ApplicationRecord
   has_many :passengers, through: :reservation_passengers, source: :student
   has_one :vehicle_report
   before_destroy :car_reservation_cancel
+  before_update :check_number_of_non_uofm_passengers
   
   has_rich_text :note
 
@@ -53,7 +56,11 @@ class Reservation < ApplicationRecord
   end
 
   def added_people
-    self.passengers.count + (self.driver.present? ? 1 : 0).to_i + (self.backup_driver.present? ? 1 : 0).to_i  
+    number = self.passengers.count + (self.driver.present? ? 1 : 0).to_i + (self.backup_driver.present? ? 1 : 0).to_i
+    if self.program.non_uofm_passengers
+      number += self.number_of_non_uofm_passengers
+    end
+    return number
   end
 
   def check_number_of_people_on_trip
@@ -82,6 +89,12 @@ class Reservation < ApplicationRecord
     ReservationMailer.car_reservation_cancel_admin(self, cancel_passengers, cancel_emails).deliver_now
     if self.driver_id.present?
       ReservationMailer.car_reservation_cancel_student(self, cancel_passengers, cancel_emails).deliver_now
+    end
+  end
+
+  def check_number_of_non_uofm_passengers
+    unless self.number_of_non_uofm_passengers.present?
+      self.number_of_non_uofm_passengers = 0
     end
   end
 
