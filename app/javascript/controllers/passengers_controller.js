b23364488f60c306b779cbacea19970087b90005
Margarita Barvinok <brita@umich.edu>
2023-07-10 09:20:09 -0400

    add more validations

--- app/javascript/controllers/passengers_controller.js
+++ app/javascript/controllers/passengers_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ['form', 'number_of_people_on_trip', 'number_of_non_uofm_passengers',
-    'current_number_of_non_uofm_passengers']
+    'current_number_of_non_uofm_passengers', 'non_uofm_passengers']
   connect() {
     console.log("connect - passenger")
   }
@@ -12,9 +12,16 @@ export default class extends Controller {
     var number_on_trip = Number(this.number_of_people_on_tripTarget.value)
     var current_non_uofm_on_trip = Number(this.current_number_of_non_uofm_passengersTarget.value)
     var non_uofm_number = Number(this.number_of_non_uofm_passengersTarget.value)
+    var non_uofm_passengers = this.non_uofm_passengersTarget.value
     var error_place = document.getElementById('error_place')
 
-    if (added_people - current_non_uofm_on_trip + non_uofm_number > number_on_trip) {
+    if (non_uofm_number > 0 && non_uofm_passengers == ''){
+      error_place.innerHTML = "The empty passengers list doesn't match the non-zero number"
+      event.preventDefault()
+    } else if (non_uofm_number == 0 && non_uofm_passengers != '' ) {
+      error_place.innerHTML = "Zero number doesn't match the non-empty passengers list"
+      event.preventDefault()
+    } else if (added_people - current_non_uofm_on_trip + non_uofm_number > number_on_trip) {
       error_place.innerHTML = added_people + " are people already added to the trip, there is no space for " +
       non_uofm_number + " non UofM passenger(s)"
       event.preventDefault()
