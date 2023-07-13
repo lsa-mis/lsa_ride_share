bfebe6df3f98a959f54c1e9a1fad51365205af1d
Margarita Barvinok <brita@umich.edu>
2023-07-13 14:42:26 -0400

    edit validations

--- app/javascript/controllers/vehiclereport_controller.js
+++ app/javascript/controllers/vehiclereport_controller.js
@@ -10,10 +10,8 @@ export default class extends Controller {
   submitForm(event) {
     
     var gas_start = this.gas_startTarget.value
-
-    var mileage_start = this.mileage_startTarget.value
-    var mileage_end = this.mileage_endTarget.value
-
+    var mileage_start = Number(this.mileage_startTarget.value)
+    var mileage_end = Number(this.mileage_endTarget.value)
     var mileage_error_place = document.getElementById('mileage_show_error')
     var gas_error_place = document.getElementById('gas_error')
     var error_scroll_place = document.getElementById('error_scroll_place')
@@ -29,19 +27,18 @@ export default class extends Controller {
       gas_error_place.innerHTML = ''
     }
 
+    mileage_error_place.innerHTML = ''
     if (mileage_start < 0 || mileage_end < 0) {
       mileage_error_place.innerHTML = "Mileage needs to be a postive value. Please enter a valid value."
       error_scroll_place.scrollIntoView()
       submitForm = false
     }
-    else if(mileage_end < mileage_start && mileage_end > 0) {
+    if(mileage_end > 0 && mileage_end < mileage_start) {
       mileage_error_place.innerHTML = "End mileage should be higher than start mileage. Please enter a valid value."
       error_scroll_place.scrollIntoView()
       submitForm = false
-    } else {
-      mileage_error_place.innerHTML = ''
-    }   
-   
+    }
+
     if(submitForm == false) {
       event.preventDefault()
     }
