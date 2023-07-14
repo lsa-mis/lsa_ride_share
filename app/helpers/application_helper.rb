27c751c835cf9da0abfe0f805c6b30c6eee0230e
Jocelyn Anderson <jocelynm@umich.edu>
2023-07-14 13:49:17 -0400

    Added reservation dates to listing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -142,6 +142,10 @@ module ApplicationHelper
     end
   end
 
+  def show_reservation_date(reservation)
+    show_date_time(reservation.start_time) + " - " +  show_date_time(reservation.end_time)
+  end
+
   def show_reserved_by_in_week_calendar(reservation)
     User.find(reservation.reserved_by).display_name
   end
