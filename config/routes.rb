c863bdfc050348bdb8354dc912d55a8d1ec4d795
Margarita Barvinok <brita@umich.edu>
2023-07-15 19:02:42 -0400

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,7 @@ Rails.application.routes.draw do
     resources :vehicle_reports, module: :reservations
   end
   get '/reservations/get_available_cars/:unit_id/:day_start/:number/:start_time/:end_time', to: 'reservations#get_available_cars'
-  get '/reservations/get_available_cars_long/:unit_id/:day_start/:day_end/:number/:start_time/:end_time', to: 'reservations#get_available_cars_long'
+  get '/reservations/get_available_cars_long/:unit_id/:day_start/:day_end/:number', to: 'reservations#get_available_cars_long'
   get '/reservations/edit_change_day/:unit_id/:day_start', to: 'reservations#edit_change_day'
   get '/reservations/add_passengers/:reservation_id', to: 'reservations/passengers#add_passengers', as: :add_passengers
   get '/reservations/add_passenger/:reservation_id', to: 'reservations/passengers#add_passenger', as: :add_passenger
