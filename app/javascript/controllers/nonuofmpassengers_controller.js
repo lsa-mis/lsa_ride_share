93eb1d082b922d43699c0b3dc9be2a999edf8c4f
Margarita Barvinok <brita@umich.edu>
2023-07-19 11:30:36 -0400

    remove console.log

--- app/javascript/controllers/nonuofmpassengers_controller.js
+++ app/javascript/controllers/nonuofmpassengers_controller.js
@@ -3,14 +3,12 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ['form', 'number_of_non_uofm_passengers', 'non_uofm_passengers']
   connect() {
-    console.log("connect - passenger")
+    console.log("connect - non uofm passenger")
   }
 
   submitForm(event) {
     var non_uofm_number = Number(this.number_of_non_uofm_passengersTarget.value)
-    console.log(non_uofm_number)
     var non_uofm_passengers = this.non_uofm_passengersTarget.value
-    console.log(non_uofm_passengers)
     var error_place = document.getElementById('error_place')
 
     if (non_uofm_number > 0 && non_uofm_passengers == ''){
