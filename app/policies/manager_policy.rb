92f86b0db027c9137431bb9d8117107ba11d1104
Margarita Barvinok <brita@umich.edu>
2023-07-25 10:03:36 -0400

    controller and policy

--- app/policies/manager_policy.rb
+++ app/policies/manager_policy.rb
@@ -2,6 +2,10 @@
 
 class ManagerPolicy < ApplicationPolicy
 
+  def index?
+    user_in_access_group?
+  end
+
   def create?
     user_in_access_group?
   end
@@ -10,7 +14,19 @@ class ManagerPolicy < ApplicationPolicy
     create?
   end
 
+  def edit?
+    update?
+  end
+
+  def update?
+    user_in_access_group?
+  end
+
   def edit_program_managers?
+    create?
+  end
+
+  def update_managers_mvr_status?
     create?
   end
 
