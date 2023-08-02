51be9340a4f9f34e757eb3c86e53c5d1b5c567cd
Margarita Barvinok <brita@umich.edu>
2023-08-02 12:04:11 -0400

    improve reservation approve validation

--- app/javascript/controllers/approve_controller.js
+++ app/javascript/controllers/approve_controller.js
@@ -12,10 +12,13 @@ export default class extends Controller {
     var driver = document.getElementById("driver").textContent
     var approve_error = document.getElementById("approve_error")
     var check = document.getElementById("reservation_approved")
-    if (car.includes("No car selected") || driver.includes("No driver selected")) {
-      approve_error.innerHTML = "Do not approve if a car or a driver is not selected"
-      check.checked = false
-      event.preventDefault()
+    console.log(check.checked)
+    if (check.checked) {
+      if (car.includes("No car selected") || driver.includes("No driver selected")) {
+        approve_error.innerHTML = "Do not approve if a car or a driver is not selected"
+        check.checked = false
+        event.preventDefault()
+      }
     } else {
       approve_error.innerHTML = ""
       Turbo.navigator.submitForm(this.formTarget)
