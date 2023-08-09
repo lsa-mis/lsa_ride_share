1519f00b6b437087c3f859842fd2c4ac78d7cebb
Margarita Barvinok <brita@umich.edu>
2023-08-09 16:34:53 -0400

    do not need that here

--- app/javascript/controllers/approve_controller.js
+++ app/javascript/controllers/approve_controller.js
@@ -1,5 +1,4 @@
 import { Controller } from "@hotwired/stimulus"
-import { get } from "@rails/request.js"
 export default class extends Controller {
   static targets = ['form', 'form1', 'cancel_type']
   
