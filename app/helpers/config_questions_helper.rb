e551197ddc6358aa05554c330507e34eadc47d08
Margarita Barvinok <brita@umich.edu>
2023-08-16 14:25:36 -0400

    add question to survey

--- app/helpers/config_questions_helper.rb
+++ app/helpers/config_questions_helper.rb
@@ -10,7 +10,8 @@ module ConfigQuestionsHelper
       "<div>Type of car reservation&nbsp;</div><ul><li>Recurring</li><li>Sporadic</li><li>Events</li><li>Trips</li><li>Other</li></ul>",
       "Does The Course Have A $50 Lab Fee?",
       "<div>What training option would be best for your course? (~30-minutes needed)&nbsp;</div><ul><li>Whole Class In-Person (Recommended)&nbsp;</li><li>Whole Class Via Zoom (Recommended)&nbsp;</li><li>Small Groups Via Zoom&nbsp;</li><li>Individuals Sign Up Online&nbsp;</li><li>Other</li></ul>",
-      "If whole class training is possible, what date would be best? (The admin will contact to coordinate time/location)"
+      "If whole class training is possible, what date would be best? (The admin will contact to coordinate time/location)",
+      "Additional comments. (Add section numbers here if the program has more than one section.)"
     ]
   end
 end
