bb70fd7d290fe567eade36e7e5dd91f9e1630140
rsmokeUM <rsmoke@umich.edu>
2023-10-19 10:45:11 -0400

    stub out login test

---
+++ spec/login/initial_login_spec.rb
@@ -0,0 +1,8 @@
+RSpec.describe "Logging in", type: :login do
+  pending "add some examples to (or delete) #{__FILE__}"
+  # scenario "user logs in with provider" do
+  #   visit "/login"
+  #   click_link "Log in with Provider"
+  #   expect(page).to have_content("Welcome, Test User!")
+  # end
+end
