748bc375368e4931a9805f9c4bacbbe85313d923
Margarita Barvinok <brita@umich.edu>
2023-11-27 10:51:40 -0500

    add course uniqueness validation

--- app/models/course.rb
+++ app/models/course.rb
@@ -13,12 +13,22 @@ class Course < ApplicationRecord
 
   before_save :upcase_subject
 
+  validates_presence_of :subject, :catalog_number, :class_section, :program_id
+  validate :course_uniqueness_in_program
+
   def display_name
     "#{self.subject} #{self.catalog_number} - #{self.class_section}"
   end
 
   def upcase_subject
     self.subject = subject.upcase
+  end
+
+  def course_uniqueness_in_program
+    if Course.where("UPPER(subject) = ? AND catalog_number = ? AND class_section = ? AND program_id = ?",
+        self.subject.upcase, self.catalog_number, self.class_section, self.program_id).exists?
+      errors.add(:program_id, "already has this course")
+    end
   end
 
 end
