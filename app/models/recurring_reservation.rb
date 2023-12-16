37bfaddc5ef69a71a13780e97685a986b1a2759d
Margarita Barvinok <brita@umich.edu>
2023-12-16 15:21:34 -0500

    remove logger

--- app/models/recurring_reservation.rb
+++ app/models/recurring_reservation.rb
@@ -76,11 +76,8 @@ class RecurringReservation
         next_reservation.prev = prev_reserv.id
         # check if there are start_time..end_time for @reservation.car is available on start_day
         # ranges = available_ranges(@reservation.car, start_day, @reservation.program.unit)
-        Rails.logger.debug "************************ inside create_all "
-        Rails.logger.debug "************************ range #{next_reservation.start_time..next_reservation.end_time}"
         unless available?(@reservation.car, next_reservation.start_time..next_reservation.end_time)
-          conflict_days_message += day.to_date.to_s + "; "
-          Rails.logger.debug "************************ conflict_day: #{day.to_date.to_s}"
+          conflict_days_message += show_date_with_month_name(@reservation.start_time) + "; "
         end
         next_reservation.save
         if prev_reserv.passengers.present?
