5fa0df23326583dc243c25e8bb85c2cb636ccb1b
Margarita Barvinok <brita@umich.edu>
2024-01-16 22:54:05 -0500

    refactor faculty_mailer

--- app/mailers/faculty_mailer.rb
+++ app/mailers/faculty_mailer.rb
@@ -1,24 +1,22 @@
 class FacultyMailer < ApplicationMailer
-  before_action :set_faculty_survey, only: [:send_faculty_survey, :faculty_survey_program_created, :faculty_survey_confirmation]
+  before_action :set_faculty_survey
 
   def send_faculty_survey(user)
     @recipient = @faculty_survey.uniqname + '@umich.edu'
     mail(to: @recipient, subject: "RideShare program: please fill out the survey" )
-    EmailLog.create(sent_from_model: "FacultySurvey", record_id: @faculty_survey.id, email_type: "send_faculty_survey",
-      sent_to: @recipient, sent_by: user.id, sent_at: DateTime.now)
+    create_email_log_records(email_type: "send_faculty_survey",)
   end
 
-  def faculty_survey_program_created(user)
+  def faculty_survey_program_created
+    @recipient = @unit_email
     mail(to: @unit_email, subject: "RideShare program: new program created from faculty survey" )
-    EmailLog.create(sent_from_model: "FacultySurvey", record_id: @faculty_survey.id, email_type: "faculty_survey_program_created",
-      sent_to: @unit_email, sent_by: user.id, sent_at: DateTime.now)
+    create_email_log_records(email_type: "faculty_survey_program_created")
   end
 
-  def faculty_survey_confirmation(user)
+  def faculty_survey_confirmation
     @recipient = @faculty_survey.uniqname + '@umich.edu'
     mail(to: @recipient, subject: "RideShare program: faculty survey submitted" )
-    EmailLog.create(sent_from_model: "FacultySurvey", record_id: @faculty_survey.id, email_type: "faculty_survey_confirmation",
-      sent_to: @recipient, sent_by: user.id, sent_at: DateTime.now)
+    create_email_log_records(email_type: "faculty_survey_confirmation")
   end
 
   private 
@@ -27,6 +25,11 @@ class FacultyMailer < ApplicationMailer
     @faculty_survey = params[:faculty_survey]
     @contact_phone = @faculty_survey.unit.unit_preferences.find_by(name: "contact_phone").value.presence || ""
     @unit_email = @faculty_survey.unit.unit_preferences.find_by(name: "notification_email").value.presence || "lsa-rideshare-admins@umich.edu"
+  end
+
+  def create_email_log_records(sent_from_model:, email_type:)
+    EmailLog.create(sent_from_model: "FacultySurvey", record_id: @faculty_survey.id, email_type: email_type,
+      sent_to: @recipient, sent_by: params[:user].id, sent_at: DateTime.now)
   end
 
 end
