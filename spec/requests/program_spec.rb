6f96753998c428713eeb7b04582e2a33953adc8f
rsmokeUM <rsmoke@umich.edu>
2024-01-18 12:16:49 -0500

    clean up code syntax

--- spec/requests/program_spec.rb
+++ spec/requests/program_spec.rb
@@ -1,30 +1,24 @@
 require 'rails_helper'
 #  Written with chatGPT and Jason Swett
 RSpec.describe "Programs", type: :request do
+  before do
+    user = FactoryBot.create(:user)
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:saml] = OmniAuth::AuthHash.new({
+      provider: 'saml',
+      uid: '123456',
+      info: {
+        email: user.email,
+        name: user.display_name,
+        uniqname: user.uniqname
+      }
+    })
+    post user_saml_omniauth_callback_path
+  end 
+
   describe 'login success' do
-    it 'displays welcome message on programs page after successful login' do
-      user = FactoryBot.create(:user)
- 
-      # Setup mock auth hash here or in a support file
-      OmniAuth.config.test_mode = true
-      OmniAuth.config.mock_auth[:saml] = OmniAuth::AuthHash.new({
-        provider: 'saml',
-        uid: '123456',
-        info: {
-          email: user.email,
-          name: user.display_name,
-          uniqname: user.uniqname,
-          # ... other attributes ...
-        }
-      })
-
-      # Simulate login by setting session variables
-      post user_saml_omniauth_callback_path # Replace with your actual callback path
-
-      # Now access the protected page
+    it 'displays welcome message on programs page' do
       get programs_path
-
-      # Follow the redirect if one occurs
       follow_redirect!
       # puts response.body
       expect(response.body).to include("Welcome")
