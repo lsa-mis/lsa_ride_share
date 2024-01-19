c98460c5e16c555fbe59bd5cf4ca861ec40779e3
Margarita Barvinok <brita@umich.edu>
2024-01-19 09:02:42 -0500

    add init model specs

--- spec/models/unit_spec.rb
+++ spec/models/unit_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Unit, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "the Factory" do
+    it 'is valid' do
+      expect(build(:unit)).to be_valid
+    end
+  end
+
+  context "create unit with all required fields present" do
+    it 'is valid' do
+      expect(create(:unit)).to be_valid
+    end
+  end
+
+  context "create unit without a name" do
+    it 'raise error "ActiveRecord::RecordInvalid: Validation failed: Name can\'t be blank"' do
+      expect { FactoryBot.create(:unit, name: nil) }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Name can't be blank")
+    end
+  end
+
+  context "create unit without a ldap_group" do
+    it 'raise error "ActiveRecord::RecordInvalid: Validation failed: Ldap group can\'t be blank"' do
+      expect { FactoryBot.create(:unit, ldap_group: nil) }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Ldap group can't be blank")
+    end
+  end
+
 end
