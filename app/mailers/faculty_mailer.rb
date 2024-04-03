ef4957611f9fdbde5a25306adc7f9bfe1e015beb
Margarita Barvinok <brita@umich.edu>
2024-04-03 18:35:30 -0400

    fix faculty survey mailer

--- app/mailers/faculty_mailer.rb
+++ app/mailers/faculty_mailer.rb
@@ -26,7 +26,7 @@ class FacultyMailer < ApplicationMailer
     @unit_email = @faculty_survey.unit.unit_preferences.find_by(name: "notification_email").value.presence || "lsa-rideshare-admins@umich.edu"
   end
 
-  def create_email_log_records(sent_from_model:, email_type:)
+  def create_email_log_records(email_type:)
     EmailLog.create(sent_from_model: "FacultySurvey", record_id: @faculty_survey.id, email_type: email_type,
       sent_to: @recipient, sent_by: params[:user].id, sent_at: DateTime.now)
   end
