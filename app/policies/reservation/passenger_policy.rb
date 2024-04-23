c2de88393443f9bf57a5ff320209178e249a00ed
Margarita Barvinok <brita@umich.edu>
2024-04-23 11:41:29 -0400

    edit policy

--- app/policies/reservation/passenger_policy.rb
+++ app/policies/reservation/passenger_policy.rb
@@ -1,4 +1,11 @@
-class  Reservation::PassengerPolicy < ApplicationPolicy
+class  Reservation::PassengerPolicy < ReservationPolicy
+
+  def initialize(context, record)
+    @user = context[:user]
+    @params = context[:params]
+    @record = record
+    @record = Reservation.find(@params[:reservation_id])
+  end
 
   def add_passengers?
     user_in_access_group? || is_in_reservation?
@@ -29,27 +36,27 @@ class  Reservation::PassengerPolicy < ApplicationPolicy
   end
 
   def is_in_reservation?
-    reservation = Reservation.find(params[:reservation_id])
+    # reservation = Reservation.find(params[:reservation_id])
     if is_student?
-      student = Student.find_by(program_id: reservation.program, uniqname: @user.uniqname)
-      return reservation.driver == student || reservation.backup_driver == student || reservation.passengers.include?(student)
+      student = Student.find_by(program_id: @record.program, uniqname: @user.uniqname)
+      return @record.driver == student || @record.backup_driver == student || @record.passengers.include?(student)
     end
     if is_manager?
       manager = Manager.find_by(uniqname: @user.uniqname)
-      return reservation.driver_manager == manager || is_reserved_by? || reservation.passengers_managers.include?(manager)
+      return @record.driver_manager == manager || is_reserved_by? || @record.passengers_managers.include?(manager)
     end
     return false
   end
 
   def is_not_driver?
-    reservation = Reservation.find(params[:reservation_id])
+    # @record = @Record.find(params[:@record_id])
     if is_student?
-      student = Student.find_by(program_id: reservation.program, uniqname: @user.uniqname)
-      return reservation.passengers.include?(student)
+      student = Student.find_by(program_id: @record.program, uniqname: @user.uniqname)
+      return @record.passengers.include?(student)
     end
     if is_manager?
       manager = Manager.find_by(uniqname: @user.uniqname)
-      return is_reserved_by? || reservation.passengers_managers.include?(manager)
+      return is_reserved_by? || @record.passengers_managers.include?(manager)
     end
     return false
   end
