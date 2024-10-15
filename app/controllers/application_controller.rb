b6f2c714d02c20b94ceee336d43291d77f62b632
Margarita Barvinok <brita@umich.edu>
2024-10-15 18:05:50 -0400

    remove before _action set_unit_and_membership

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   include ApplicationHelper
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-  before_action :set_unit_and_membership
   after_action :verify_authorized, unless: :devise_controller?
   skip_after_action :verify_authorized, only: [:delete_file_attachment]
 
@@ -23,18 +22,6 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def set_unit_and_membership
-    if user_signed_in?
-      current_user.membership = session[:user_memberships]
-      if is_super_admin?(current_user)
-        current_user.unit_ids = Unit.all.pluck(:id)
-      else
-        current_user.unit_ids = Unit.where(ldap_group: session[:user_memberships]).pluck(:id)
-      end
-    else
-      new_user_session_path
-    end
-  end
 
   def after_sign_in_path_for(resource)
     if $baseURL.present?
