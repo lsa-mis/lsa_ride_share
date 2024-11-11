8d4bafd764d30d261305226f626668bea7e079df
Margarita Barvinok <brita@umich.edu>
2024-11-10 21:11:14 -0500

    finish welcome page policy

--- spec/policies/welcome_page_policy_spec.rb
+++ spec/policies/welcome_page_policy_spec.rb
@@ -2,41 +2,37 @@ require 'rails_helper'
 
 RSpec.describe WelcomePagePolicy, type: :policy do
   let(:user) { FactoryBot.create(:user) }
-  let(:user_manager) { FactoryBot.create(:user) }
-  let(:manager) { FactoryBot.create(:manager, uniqname: user_manager.uniqname) }
-  let(:user_student) { FactoryBot.create(:user) }
-  let(:student) { FactoryBot.create(:student, uniqname: user_student.uniqname, program: program) }
 
   context 'with super_admin role' do
     subject { described_class.new({ user: user, role: "super_admin" }, :welcome_page) }
 
-    it { is_expected.to forbid_actions(%i[is_admin is_student is_manager add_student_phone edit_student_phone add_manager_phone edit_manager_phone]) }
-    it { is_expected.to permit_only_actions(%i[is_super_admin user_in_access_group get_instructor_id]) }
+    it { is_expected.to forbid_actions(%i[is_admin is_student is_manager add_student_phone edit_student_phone add_manager_phone edit_manager_phone manager student]) }
+    it { is_expected.to permit_only_actions(%i[is_super_admin user_in_access_group]) }
   end
 
   context 'with admin role' do
-    subject { described_class.new({ user: user, role: "admin" }, car) }
+    subject { described_class.new({ user: user, role: "admin" }, :welcome_page) }
 
-    it { is_expected.to forbid_actions(%i[is_super_admin is_student is_manager]) }
-    it { is_expected.to permit_only_actions(%i[is_admin user_in_access_group index show create new update edit get_parking_locations delete_file get_instructor_id]) }
+    it { is_expected.to forbid_actions(%i[is_super_admin is_student is_manager add_student_phone edit_student_phone add_manager_phone edit_manager_phone manager student]) }
+    it { is_expected.to permit_only_actions(%i[is_admin user_in_access_group]) }
   end
 
   context 'with manager role' do
-    subject { described_class.new({ user: user, role: "manager" }, car) }
+    subject { described_class.new({ user: user, role: "manager" }, :welcome_page) }
 
-    it { is_expected.to forbid_actions(%i[is_super_admin is_admin is_student index create new update edit get_parking_locations delete_file get_instructor_id]) }
-    it { is_expected.to permit_only_actions(%i[is_manager show]) }
+    it { is_expected.to forbid_actions(%i[is_super_admin is_admin is_student user_in_access_group add_student_phone edit_student_phone student]) }
+    it { is_expected.to permit_only_actions(%i[is_manager add_manager_phone edit_manager_phone manager]) }
   end
 
   context 'with student role' do
-    subject { described_class.new({ user: user, role: "student" }, car) }
+    subject { described_class.new({ user: user, role: "student" }, :welcome_page) }
 
-    it { is_expected.to forbid_actions(%i[is_super_admin is_admin is_manager index create new update edit get_parking_locations delete_file get_instructor_id]) }
-    it { is_expected.to permit_only_actions(%i[is_student show]) }
+    it { is_expected.to forbid_actions(%i[is_super_admin is_admin is_manager user_in_access_group add_manager_phone edit_manager_phone manager]) }
+    it { is_expected.to permit_only_actions(%i[is_student add_student_phone edit_student_phone student]) }
   end
 
   context 'with no role' do
-    subject { described_class.new({ user: user, role: "none" }, car) }
+    subject { described_class.new({ user: user, role: "none" }, :welcome_page) }
 
     it { is_expected.to forbid_all_actions }
   end
