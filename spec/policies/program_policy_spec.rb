a990221f8d5160a2e8be88d8f1247fe3293d523d
Margarita Barvinok <brita@umich.edu>
2024-11-10 21:17:18 -0500

    fix program policy spec

--- spec/policies/program_policy_spec.rb
+++ spec/policies/program_policy_spec.rb
@@ -2,34 +2,35 @@ require 'rails_helper'
 
 RSpec.describe ProgramPolicy, type: :policy do
   let(:user) { FactoryBot.create(:user) }
-  let(:manager) { FactoryBot.create(:manager, uniqname: user.uniqname) }
+  let(:user_manager) { FactoryBot.create(:user) }
+  let(:manager) { FactoryBot.create(:manager, uniqname: user_manager.uniqname) }
   let(:program) { FactoryBot.create(:program, instructor: manager) }
 
   context 'with super_admin role' do
     subject { described_class.new({ user: user, role: "super_admin", params: {id: program.id} }, program) }
 
     it { is_expected.to forbid_actions(%i[is_admin is_student is_manager]) }
-    it { is_expected.to permit_only_actions(%i[is_super_admin user_in_access_group index show create new update edit duplicate get_programs_list get_students_list get_sites_list get_instructor_id]) }
+    it { is_expected.to permit_only_actions(%i[is_super_admin user_in_access_group index show create new update edit duplicate get_programs_list get_students_list get_sites_list]) }
   end
 
   context 'with admin role' do
     subject { described_class.new({ user: user, role: "admin", params: {id: program.id} }, program) }
 
     it { is_expected.to forbid_actions(%i[is_super_admin is_student is_manager]) }
-    it { is_expected.to permit_only_actions(%i[is_admin user_in_access_group index show create new update edit duplicate get_programs_list get_students_list get_sites_list get_instructor_id]) }
+    it { is_expected.to permit_only_actions(%i[is_admin user_in_access_group index show create new update edit duplicate get_programs_list get_students_list get_sites_list]) }
   end
 
   context 'with manager role' do
-    subject { described_class.new({ user: user, role: "manager", params: {id: program.id} }, program) }
+    subject { described_class.new({ user: user_manager, role: "manager", params: {id: program.id} }, program) }
 
-    it { is_expected.to forbid_actions(%i[is_super_admin is_student is_admin create new get_instructor_id]) }
+    it { is_expected.to forbid_actions(%i[is_super_admin is_student is_admin create new]) }
     it { is_expected.to permit_only_actions(%i[is_manager is_instructor is_program_manager index show update edit duplicate]) }
   end
 
   context 'with student role' do
     subject { described_class.new({ user: user, role: "student", params: {id: program.id} }, program) }
 
-    it { is_expected.to forbid_actions(%i[is_super_admin is_admin is_manager index show create new update edit duplicate get_programs_list get_students_list get_sites_list get_instructor_id]) }
+    it { is_expected.to forbid_actions(%i[is_super_admin is_admin is_manager index show create new update edit duplicate get_programs_list get_students_list get_sites_list]) }
     it { is_expected.to permit_only_actions(%i[is_student]) }
   end
 
