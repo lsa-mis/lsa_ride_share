71b1e5fffb7f6f1812dfbfe4e39b7d44cb3746a9
Margarita Barvinok <brita@umich.edu>
2024-11-26 12:44:23 -0500

    remove vcr and webmock

--- spec/system/cars_spec.rb
+++ spec/system/cars_spec.rb
@@ -12,35 +12,31 @@ RSpec.describe Car, type: :system do
 
 	context "create new car" do
     it 'is valid input' do
-      # VCR.use_cassette "car" do
-        unit = Unit.first
-        visit cars_path
-        click_on "New Car"
-        fill_in "Car Number", with: "12345"
-        fill_in "Make", with: "Ford"
-        fill_in "Model", with: "Fusion"
-        fill_in "Color", with: "white"
-        fill_in "Number of Seats", with: "5"
-        fill_in "Mileage", with: "10000"
-        select "50.0", from: "Percent of Fuel Remaining"
-        select "Thayer 1A", :from => "parking_spot_select"
-        click_on "Create Car"
-        expect(page).to have_content("A new car was added.")
-      # end
+      unit = Unit.first
+      visit cars_path
+      click_on "New Car"
+      fill_in "Car Number", with: "12345"
+      fill_in "Make", with: "Ford"
+      fill_in "Model", with: "Fusion"
+      fill_in "Color", with: "white"
+      fill_in "Number of Seats", with: "5"
+      fill_in "Mileage", with: "10000"
+      select "50.0", from: "Percent of Fuel Remaining"
+      select "Thayer 1A", :from => "parking_spot_select"
+      click_on "Create Car"
+      expect(page).to have_content("A new car was added.")
     end
   end
 
   context "edit a car" do
     it 'updates parking location' do
-      VCR.use_cassette "car" do
-        car = FactoryBot.create(:car, updated_by: User.last.id, unit: Unit.last)
-        visit "cars/#{car.id}/"
-        click_on "Edit Car"
-        select "Thayer 1A", :from => "parking_spot_select"
-        click_on "Update Car"
-        expect(page).to have_content("Thayer 1A")
-        expect(page).to have_content("The car was updated.")
-      end
+      car = FactoryBot.create(:car, updated_by: User.last.id, unit: Unit.last)
+      visit "cars/#{car.id}/"
+      click_on "Edit Car"
+      select "Thayer 1A", :from => "parking_spot_select"
+      click_on "Update Car"
+      expect(page).to have_content("Thayer 1A")
+      expect(page).to have_content("The car was updated.")
     end
   end
 
