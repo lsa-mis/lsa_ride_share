d8408aeb53a546dc91b71e5bd1e17c584fa3a17d
Margarita Barvinok <brita@umich.edu>
2024-12-18 22:36:47 -0500

    refactor vehicle report spec

--- spec/policies/vehicle_report_policy_spec.rb
+++ spec/policies/vehicle_report_policy_spec.rb
@@ -8,19 +8,12 @@ RSpec.describe VehicleReportPolicy, type: :policy do
   let(:car) { FactoryBot.create(:car, unit: unit) }
   let(:site) { FactoryBot.create(:site, unit: unit) }
   let(:program) { FactoryBot.create(:program, unit: unit, instructor: manager) }
-  let(:user_student) { FactoryBot.create(:user) }
-  let(:student) { FactoryBot.create(:student, uniqname: user_student.uniqname, program: program) }
-  let(:reservation) { FactoryBot.create(:reservation, program: program, site: site, car: car, reserved_by: user.id) }
-  let(:reservation_manager) { FactoryBot.create(:reservation, program: program, site: site, car: car, driver_manager_id: manager.id, reserved_by: user_manager.id) }
-  let(:reservation_student) { FactoryBot.create(:reservation, program: program, site: site, car: car, driver_id: student.id, reserved_by: user_student.id) }
-  let(:user_none) { FactoryBot.create(:user) }
-  let(:vehicle_report) { FactoryBot.create(:vehicle_report, reservation: reservation) }
-  let(:vehicle_report_manager) { FactoryBot.create(:vehicle_report, reservation: reservation_manager) }
-  let(:vehicle_report_student) { FactoryBot.create(:vehicle_report, reservation: reservation_student) }
-
 
   context 'with super_admin role' do
-    subject { described_class.new({ user: user, role: "super_admin", params: {id: vehicle_report.id, reservation_id: reservation_manager.id} }, vehicle_report) }
+    let(:reservation) { FactoryBot.create(:reservation, program: program, site: site, car: car, reserved_by: user.id) }
+    let(:vehicle_report) { FactoryBot.create(:vehicle_report, reservation: reservation) }
+
+    subject { described_class.new({ user: user, role: "super_admin", params: {id: vehicle_report.id, reservation_id: reservation.id} }, vehicle_report) }
 
     it { is_expected.to forbid_actions(%i[]) }
     it { is_expected.to permit_only_actions(%i[index show create new update edit upload_image delete_image destroy
@@ -28,7 +21,10 @@ RSpec.describe VehicleReportPolicy, type: :policy do
   end
 
   context 'with admin role' do
-    subject { described_class.new({ user: user, role: "admin", params: {id: vehicle_report.id, reservation_id: reservation_manager.id} }, vehicle_report) }
+    let(:reservation) { FactoryBot.create(:reservation, program: program, site: site, car: car, reserved_by: user.id) }
+    let(:vehicle_report) { FactoryBot.create(:vehicle_report, reservation: reservation) }
+
+    subject { described_class.new({ user: user, role: "admin", params: {id: vehicle_report.id, reservation_id: reservation.id} }, vehicle_report) }
 
     it { is_expected.to forbid_actions(%i[]) }
     it { is_expected.to permit_only_actions(%i[index show create new update edit upload_image delete_image destroy
@@ -36,6 +32,9 @@ RSpec.describe VehicleReportPolicy, type: :policy do
   end
 
   context 'with manager role' do
+    let(:reservation_manager) { FactoryBot.create(:reservation, program: program, site: site, car: car, driver_manager_id: manager.id, reserved_by: user_manager.id) }
+    let(:vehicle_report_manager) { FactoryBot.create(:vehicle_report, reservation: reservation_manager) }
+
     subject { described_class.new({ user: user_manager, role: "manager", params: {id: vehicle_report_manager.id, reservation_id: reservation_manager.id} }, vehicle_report_manager) }
 
     it { is_expected.to forbid_actions(%i[index upload_damage_form delete_damage_form download_vehicle_damage_form]) }
@@ -44,6 +43,11 @@ RSpec.describe VehicleReportPolicy, type: :policy do
   end
 
   context 'with student role' do
+    let(:user_student) { FactoryBot.create(:user) }
+    let(:student) { FactoryBot.create(:student, uniqname: user_student.uniqname, program: program) }
+    let(:reservation_student) { FactoryBot.create(:reservation, program: program, site: site, car: car, driver_id: student.id, reserved_by: user_student.id) }
+    let(:vehicle_report_student) { FactoryBot.create(:vehicle_report, reservation: reservation_student) }
+
     subject { described_class.new({ user: user_student, role: "student", params: {id: vehicle_report_student.id, reservation_id: reservation_student.id} }, vehicle_report_student) }
     it { is_expected.to forbid_actions(%i[index upload_damage_form delete_damage_form download_vehicle_damage_form]) }
     it { is_expected.to permit_only_actions(%i[show create new update edit upload_image delete_image destroy
@@ -51,7 +55,11 @@ RSpec.describe VehicleReportPolicy, type: :policy do
   end
 
   context 'with no role' do
-    subject { described_class.new({ user: user_none, role: "none", params: {id: vehicle_report.id, reservation_id: reservation_manager.id}  }, reservation) }
+    let(:user_none) { FactoryBot.create(:user) }
+    let(:reservation) { FactoryBot.create(:reservation, program: program, site: site, car: car, reserved_by: user.id) }
+    let(:vehicle_report) { FactoryBot.create(:vehicle_report, reservation: reservation) }
+
+    subject { described_class.new({ user: user_none, role: "none", params: {id: vehicle_report.id, reservation_id: reservation.id}  }, reservation) }
 
     it { is_expected.to forbid_all_actions }
   end
