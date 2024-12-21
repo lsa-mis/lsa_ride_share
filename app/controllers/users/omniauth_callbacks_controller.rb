18375bfc3c7ca102a0a7e0ace03cc177578d14ce
Margarita Barvinok <brita@umich.edu>
2024-12-21 11:55:40 -0500

    refactor

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -42,20 +42,19 @@ def set_user
   if @user
     session[:user_email] = @user.email
     uniqname = @user.uniqname
-    session[:user_memberships] = []
+    user_membership = []
     access_groups = Unit.pluck(:ldap_group)
     access_groups.each do |group|
       if  LdapLookup.is_member_of_group?(uniqname, group)
-        session[:user_memberships].append(group)
+        user_membership.append(group)
       end
     end
     session[:unit_ids] = []
     if LdapLookup.is_member_of_group?(uniqname, 'lsa-was-rails-devs')
       session[:unit_ids] = Unit.all.pluck(:id)
       session[:role] = 'super_admin'
-
     else
-      if session[:user_memberships].present?
+      if user_membership.present?
         session[:role] = 'admin'
       elsif Manager.find_by(uniqname: uniqname).present?
         session[:role] = 'manager'
@@ -64,7 +63,7 @@ def set_user
       else
         session[:role] = 'none'
       end
-      session[:unit_ids] = Unit.where(ldap_group: session[:user_memberships]).pluck(:id)
+      session[:unit_ids] = Unit.where(ldap_group: user_membership).pluck(:id)
     end
     
   end
