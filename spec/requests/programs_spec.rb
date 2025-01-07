af5a20824871bffed950561e37c5a2b87a0fde3e
Margarita Barvinok <brita@umich.edu>
2025-01-07 13:40:48 -0500

    more program requests tests

--- spec/requests/programs_spec.rb
+++ spec/requests/programs_spec.rb
@@ -6,6 +6,31 @@ RSpec.describe Program, type: :request do
   # go to index action
   # expect 200
   context 'index action' do
+
+    context 'with super_admin role' do
+      let!(:user) { FactoryBot.create(:user) }
+
+      it 'returns 200' do
+        allow(LdapLookup).to receive(:is_member_of_group?).with(user.uniqname, "lsa-was-rails-devs").and_return(true)
+        allow(LdapLookup).to receive(:is_member_of_group?).with(user.uniqname, Unit.first.ldap_group).and_return(false)
+        mock_login(user)
+        get programs_path
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'with admin role' do
+      let!(:user) { FactoryBot.create(:user) }
+
+      it 'returns 200' do
+        allow(LdapLookup).to receive(:is_member_of_group?).with(user.uniqname, "lsa-was-rails-devs").and_return(false)
+        allow(LdapLookup).to receive(:is_member_of_group?).with(user.uniqname, Unit.first.ldap_group).and_return(true)
+        mock_login(user)
+        get programs_path
+        expect(response).to have_http_status(200)
+      end
+    end
+
     context 'with manager role who has a program' do
       let!(:user_manager) { FactoryBot.create(:user) }
       let!(:manager) { FactoryBot.create(:manager, uniqname: user_manager.uniqname) }
@@ -16,8 +41,6 @@ RSpec.describe Program, type: :request do
         allow(LdapLookup).to receive(:is_member_of_group?).with(user_manager.uniqname, anything).and_return(false)
         mock_login(user_manager)
         get programs_path
-        # binding.pry
-        puts response.body
         expect(response).to have_http_status(200)
       end
     end
@@ -26,14 +49,45 @@ RSpec.describe Program, type: :request do
       let!(:user_manager) { FactoryBot.create(:user) }
       let!(:manager) { FactoryBot.create(:manager, uniqname: user_manager.uniqname) }
 
-      it 'returns 302' do
+      it 'returns 302, redirects to welcome manager page and displayes alert message' do
         allow(LdapLookup).to receive(:is_member_of_group?).with(anything, "lsa-was-rails-devs").and_return(false)
         allow(LdapLookup).to receive(:is_member_of_group?).with(user_manager.uniqname, anything).and_return(false)
         mock_login(user_manager)
         get programs_path
-        # binding.pry
         expect(response).to have_http_status(302)
         expect(response.location).to include("/welcome_pages/manager")
+        expect(flash[:alert]).to match("You are not authorized to perform this action.")
+      end
+    end
+
+    context 'with student role who has a currrent term program' do
+      let!(:user_manager) { FactoryBot.create(:user) }
+      let!(:manager) { FactoryBot.create(:manager, uniqname: user_manager.uniqname) }
+      let!(:program) { FactoryBot.create(:program, instructor: manager) }
+      let!(:user_student) { FactoryBot.create(:user) }
+      let!(:student) { FactoryBot.create(:student, uniqname: user_student.uniqname, program: program) }
+
+      it 'returns 302, redirects to welcome student page and displayes alert message' do
+        allow(LdapLookup).to receive(:is_member_of_group?).with(anything, "lsa-was-rails-devs").and_return(false)
+        allow(LdapLookup).to receive(:is_member_of_group?).with(user_student.uniqname, anything).and_return(false)
+        mock_login(user_student)
+        get programs_path
+        expect(response).to have_http_status(302)
+        expect(response.location).to include("/welcome_pages/student")
+        expect(flash[:alert]).to match("You are not authorized to perform this action.")
+      end
+    end
+
+    context 'with "none" role who has no currrent term programs' do
+      let!(:user) { FactoryBot.create(:user) }
+
+      it 'returns 302, redirects to root and displayes alert message' do
+        allow(LdapLookup).to receive(:is_member_of_group?).with(anything, "lsa-was-rails-devs").and_return(false)
+        allow(LdapLookup).to receive(:is_member_of_group?).with(user.uniqname, anything).and_return(false)
+        mock_login(user)
+        get programs_path
+        expect(response).to have_http_status(302)
+        expect(response.location).to match("/")
         expect(flash[:alert]).to match("You are not authorized to perform this action.")
       end
     end
