6f20256e8297daca0eeaaef5f33de211e1a1d623
Margarita Barvinok <brita@umich.edu>
2025-08-13 15:17:49 -0400

    replace svg icons with font awesome

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -953,15 +953,10 @@ module ApplicationHelper
     return unless params[:sort].to_s == column.to_s
 
     if params[:direction] == "asc"
-      svg_icon("M5 15l7-7 7 7")
+      content_tag(:i, "", class: "fa-solid fa-caret-up ml-1")
     else
-      svg_icon("M19 9l-7 7-7-7")
+      content_tag(:i, "", class: "fa-solid fa-caret-down ml-1")
     end
   end
 
-  def svg_icon(path_d)
-    content_tag(:svg, xmlns: "http://www.w3.org/2000/svg", class: "ml-1 inline w-4 h-4", fill: "none", viewBox: "0 0 24 24", stroke: "currentColor") do
-      "<path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='#{path_d}'></path>".html_safe
-    end
-  end
 end
