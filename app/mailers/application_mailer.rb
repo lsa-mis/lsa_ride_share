c2c4cf8cf84190db2f8e959dc3120282fc2544ee
Margarita Barvinok <brita@umich.edu>
2025-09-25 19:13:40 -0400

    change the method back

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -17,18 +17,12 @@ class ApplicationMailer < ActionMailer::Base
 
   def reply_to_email
     unit = params[:reservation]&.program&.unit
+    return 'lsa-rideshare-admins@umich.edu' unless unit.present?
     if unit.present?
-      email = Rails.cache.fetch("#{cache_key(unit)}/email", expires_in: 12.hours) do
-        UnitPreference.find_by(name: "notification_email", unit_id: unit.id)&.value
-      end
+      email = UnitPreference.find_by(name: "notification_email", unit_id: unit.id)&.value
       return email if email.present?
     end
     'lsa-rideshare-admins@umich.edu'
-  end
-  
-  def cache_key(unit)
-    pref = UnitPreference.find_by(name: "notification_email", unit_id: unit.id)
-    pref.present? ? pref.cache_key_with_version : nil
   end
 
 end
