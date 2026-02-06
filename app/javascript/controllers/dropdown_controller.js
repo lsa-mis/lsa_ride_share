8e6ae8ad3e5e927323e74bef5eb69b7c282cc349
Margarita Barvinok <brita@umich.edu>
2026-02-06 10:15:03 -0500

    add keyboard navigation to header

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -1,5 +1,55 @@
-import { Application } from '@hotwired/stimulus'
-import Dropdown from 'stimulus-dropdown'
+import { Controller } from "@hotwired/stimulus"
 
-const application = Application.start()
-application.register('dropdown', Dropdown)
+export default class extends Controller {
+  static targets = ["button", "menu"]
+
+  connect() {
+    this.isOpen = false
+  }
+
+  toggle() {
+    if (this.isOpen) {
+      this.close()
+    } else {
+      this.open()
+    }
+  }
+
+  open() {
+    this.isOpen = true
+    this.menuTarget.classList.remove("invisible")
+    this.buttonTarget.setAttribute("aria-expanded", "true")
+  }
+
+  close() {
+    this.isOpen = false
+    this.menuTarget.classList.add("invisible")  
+    this.buttonTarget.setAttribute("aria-expanded", "false")
+  }
+
+  handleKeydown(event) {
+    switch (event.key) {
+      case "Enter":
+      case " ": // Space
+        event.preventDefault()
+        this.toggle()
+        break
+      case "Escape":
+        if (this.isOpen) {
+          this.close()
+        }
+        break
+    }
+  }
+
+  // Close when clicking outside
+  disconnect() {
+    document.removeEventListener("click", this.handleOutsideClick)
+  }
+
+  handleOutsideClick = (event) => {
+    if (!this.element.contains(event.target)) {
+      this.close()
+    }
+  }
+}
