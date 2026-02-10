d1a7079c1a387919178d8027f36d9f3baf301468
Margarita Barvinok <brita@umich.edu>
2026-02-10 17:55:37 -0500

    refactor

--- app/controllers/system_reports_controller.rb
+++ app/controllers/system_reports_controller.rb
@@ -400,7 +400,12 @@ class SystemReportsController < ApplicationController
           (CASE WHEN (SELECT exists(SELECT 1 from active_storage_attachments where record_type = 'VehicleReport' and name = 'image_damages' and record_id = vehicle_reports.id)) = true
             THEN 'Yes' ELSE 'No' END) AS car_damage,
           (SELECT email FROM users WHERE vehicle_reports.updated_by = users.id) AS last_updated_by,
-          COALESCE(REGEXP_REPLACE(rich_text_comments.body, E'<[^>]*>', '', 'g'), '') AS comment,
+          COALESCE((SELECT REGEXP_REPLACE(art_comments.body, E'<[^>]*>', '', 'g') 
+                    FROM action_text_rich_texts AS art_comments 
+                    WHERE art_comments.record_type = 'VehicleReport' 
+                      AND art_comments.record_id = vehicle_reports.id 
+                      AND art_comments.name = 'comment' 
+                    LIMIT 1), '') AS comment,
           COALESCE((SELECT STRING_AGG(REGEXP_REPLACE(art_notes.body, E'<[^>]*>', '', 'g'), '; ') 
                     FROM notes 
                     LEFT JOIN action_text_rich_texts AS art_notes ON art_notes.record_type = 'Note' 
@@ -412,8 +417,6 @@ class SystemReportsController < ApplicationController
         JOIN programs ON programs.id = res.program_id
         JOIN terms ON terms.id = programs.term_id
         JOIN units ON units.id = programs.unit_id
-        LEFT JOIN action_text_rich_texts AS rich_text_comments ON rich_text_comments.record_type = 'VehicleReport' 
-          AND rich_text_comments.record_id = vehicle_reports.id AND rich_text_comments.name = 'comment'
         WHERE terms.id = #{@term_id} AND  units.id = #{@unit_id}"
         if params[:program_id].present?
           sql += " AND programs.id = #{params[:program_id].to_i}"
