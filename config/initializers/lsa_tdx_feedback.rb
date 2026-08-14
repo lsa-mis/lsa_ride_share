1bb94a6cb3a2ad5574e899cba2d7ab95e0542eb3
Margarita Barvinok <brita@umich.edu>
2026-08-14 13:43:34 -0400

    refactor

--- config/initializers/lsa_tdx_feedback.rb
+++ config/initializers/lsa_tdx_feedback.rb
@@ -1,5 +1,12 @@
 Rails.application.reloader.to_prepare do
   LsaTdxFeedback::FeedbackController.class_eval do
-    before_action -> { authorize :feedback }, only: :create
+    skip_before_action :authorize_feedback, raise: false
+    before_action :authorize_feedback, only: :create
+
+    private
+    
+    def authorize_feedback
+      authorize :feedback
+    end
   end
 end
