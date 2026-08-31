49f199a0d4dd1cb52617f86fd9a622f232cdebba
Margarita Barvinok <brita@umich.edu>
2026-08-31 10:48:50 -0400

    add resquest tests for cars controller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -77,6 +77,7 @@ class CarsController < ApplicationController
   end
 
   def get_parking_locations
+    authorize Car
     parking_prefs = UnitPreference.find_by(name: "parking_location", unit_id: params[:unit_id]).value
     parking_locations = []
     if parking_prefs.present?
@@ -84,7 +85,6 @@ class CarsController < ApplicationController
       parking_locations.each(&:strip!)
     end
     render json: parking_locations
-    authorize Car
   end
 
   private
