d81c9b7fe6d98bc5909b5ebd8d86a26d07319c1e
Margarita Barvinok <brita@umich.edu>
2026-08-31 17:42:42 -0400

    refactpr method

--- spec/support/spec_helper.rb
+++ spec/support/spec_helper.rb
@@ -25,14 +25,17 @@ module SpecHelper
   end
 
   def stub_admin_access(user, unit_or_group = nil)
-    allow(LdapLookup).to receive(:is_member_of_group?).with(anything, SUPER_ADMIN_LDAP_GROUP).and_return(false)
+    # allow(LdapLookup).to receive(:is_member_of_group?).with(anything, SUPER_ADMIN_LDAP_GROUP).and_return(false)
 
     if unit_or_group.present?
       ldap_group = unit_or_group.respond_to?(:ldap_group) ? unit_or_group.ldap_group : unit_or_group
+      allow(LdapLookup).to receive(:is_member_of_group?).with(user.uniqname, anything).and_return(false)
       allow(LdapLookup).to receive(:is_member_of_group?).with(user.uniqname, ldap_group).and_return(true)
     else
       allow(LdapLookup).to receive(:is_member_of_group?).with(user.uniqname, anything).and_return(true)
     end
+    # Ensure this user is not treated as super-admin (override the broader stub above).
+    allow(LdapLookup).to receive(:is_member_of_group?).with(user.uniqname, SUPER_ADMIN_LDAP_GROUP).and_return(false)
   end
 
   def stub_non_admin_access(user)
