d777557a64c6e248896207bc5db99ddbb6cc2a5c
Margarita Barvinok <brita@umich.edu>
2026-09-17 10:17:20 -0400

    refactor vehicle_reports destroy method

--- app/controllers/vehicle_reports_controller.rb
+++ app/controllers/vehicle_reports_controller.rb
@@ -230,16 +230,16 @@ class VehicleReportsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_vehicle_report
+      @vehicle_report = VehicleReport.find(params[:id])
+      authorize @vehicle_report
+
+      return unless action_name == "destroy" && !@vehicle_report.approved
       attachment_associations = %i[
         image_front_start_attachment image_driver_start_attachment image_passenger_start_attachment image_back_start_attachment
         image_front_end_attachment image_driver_end_attachment image_passenger_end_attachment image_back_end_attachment
         image_damages_attachments damage_form_attachment
       ]
-      @vehicle_report = if action_name == "destroy"
-        VehicleReport.includes(*attachment_associations).find(params[:id])
-      else
-        VehicleReport.find(params[:id])
-      end
+      @vehicle_report = VehicleReport.includes(*attachment_associations.map { |association| { association => :blob } }).find(@vehicle_report.id)
       authorize @vehicle_report
     end
 
